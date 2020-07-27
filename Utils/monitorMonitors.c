775fe74d2911440ae1d45b680d14ec3c9c960e7d
Arthur Choung <arthur_choung@me.com>
2020-07-27 23:16:56 +1000

    Added various scripts for monitors, block devices, and network interfaces

---
+++ Utils/monitorMonitors.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <linux/netlink.h>
+
+int main()
+{
+    setbuf(stdout, NULL);
+
+    struct sockaddr_nl nl;
+    char buf[8192];
+
+    memset(&nl,0,sizeof(struct sockaddr_nl));
+    nl.nl_family = AF_NETLINK;
+    nl.nl_pid = getpid();
+    nl.nl_groups = -1;
+    int fd = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_KOBJECT_UEVENT);
+    if (fd < 0) {
+        fprintf(stderr, "Unable to create socket\n");
+        exit(1);
+    }
+
+    if (bind(fd, (void *)&nl, sizeof(struct sockaddr_nl)) != 0) {
+        fprintf(stderr, "Unable to bind socket\n");
+        exit(1);
+    }
+
+    for(;;) {
+        int len = recv(fd, buf, sizeof(buf), 0);
+        if (len < 0) {
+            fprintf(stderr, "Error reading from socket: %s", strerror(errno));
+            continue;
+        }
+        int i = 0;
+        int line = 0;
+        while (i < len) {
+            char *p = buf+i;
+            if (line == 0) {
+                if (!strncmp(p, "libudev", 7)) {
+                    break;
+                }
+            } else {
+                if (!strncmp(p, "SUBSYSTEM=drm", 13)) {
+                    printf("%s\n", p);
+                    break;
+                }
+            }
+//            printf("%d: %s\n", line, p);
+            i += strlen(buf+i)+1;
+            line++;
+        }
+    }
+
+    return 0;
+}
--- makeUtils.sh
+++ makeUtils.sh
@@ -6,4 +6,7 @@ cd Utils
 clang -o printALSAStatus printALSAStatus.c -lasound
 clang -o setALSAMute: setALSAMute:.c -lasound
 clang -o setALSAVolume setALSAVolume.c -lasound
+clang -o monitorBlockDevices monitorBlockDevices.c
+clang -o monitorNetworkInterfaces monitorNetworkInterfaces.c
+clang -o monitorMonitors monitorMonitors.c
 
