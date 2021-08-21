ac9308938629b174ffcbffae5b1d9fb75d43ae98
Arthur Choung <arthur_choung@me.com>
2021-08-21 22:46:41 +1000

    Added double click and selected

--- objects/object-AmigaDir.m
+++ objects/object-AmigaDir.m
@@ -25,6 +25,8 @@
 
 #import "HOTDOG.h"
 
+#include <sys/time.h>
+
 static id drawerPalette =
 @"b #000000\n"
 @". #000022\n"
@@ -34,36 +36,40 @@ static id drawerPalette =
 ;
 
 static id drawerPixels =
-@"           ...........................................................\n"
-@"           bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
-@"       ....oooooooooooooooooooooooooooooooooooooooooooooooooooooo...o.\n"
-@"       bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
-@"   ....oooooooooooooooooooooooooooooooooooooooooooooooooooooooo...oo..\n"
-@"   bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
-@"................................................................ooo.o.\n"
-@"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
-@"..oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo..oooo..\n"
-@"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
-@"..ooo......................................................ooo..ooo.o.\n"
-@"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
-@"..ooo..oooooooooooooooooooooooooooooooooooooooooooooooooo..ooo..oo.o..\n"
-@"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
-@"..ooo..oooooooooooooooooooooooooooooooooooooooooooooooooo..ooo..ooo.o.\n"
-@"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
-@"..ooo..ooooooooooooooooo...oooooooooo...ooooooooooooooooo..ooo..oo.o..\n"
-@"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
-@"..ooo..oooooooooooooooo................oooooooooooooooooo..ooo..o.o.. \n"
-@"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb \n"
-@"..ooo..oooooooooooooooooooooooooooooooooooooooooooooooooo..ooo..oo..  \n"
-@"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb  \n"
-@"..ooo..oooooooooooooooooooooooooooooooooooooooooooooooooo..ooo..o..   \n"
-@"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb   \n"
-@"..ooo......................................................ooo....    \n"
-@"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb    \n"
-@"..oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo...     \n"
-@"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb     \n"
-@"................................................................      \n"
-@"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb      \n"
+@"              ...........................................................\n"
+@"              bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
+@"          ....oooooooooooooooooooooooooooooooooooooooooooooooooooooo...o.\n"
+@"          bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
+@"      ....oooooooooooooooooooooooooooooooooooooooooooooooooooooooo...oo..\n"
+@"      bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
+@"   ................................................................ooo.o.\n"
+@"   bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
+@"   ..oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo..oooo..\n"
+@"   bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
+@"   ..ooo......................................................ooo..ooo.o.\n"
+@"   bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
+@"   ..ooo..oooooooooooooooooooooooooooooooooooooooooooooooooo..ooo..oo.o..\n"
+@"   bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
+@"   ..ooo..oooooooooooooooooooooooooooooooooooooooooooooooooo..ooo..ooo.o.\n"
+@"   bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
+@"   ..ooo..ooooooooooooooooo...oooooooooo...ooooooooooooooooo..ooo..oo.o..\n"
+@"   bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n"
+@"   ..ooo..oooooooooooooooo................oooooooooooooooooo..ooo..o.o.. \n"
+@"   bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb \n"
+@"   ..ooo..oooooooooooooooooooooooooooooooooooooooooooooooooo..ooo..oo..  \n"
+@"   bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb  \n"
+@"   ..ooo..oooooooooooooooooooooooooooooooooooooooooooooooooo..ooo..o..   \n"
+@"   bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb   \n"
+@"   ..ooo......................................................ooo....    \n"
+@"   bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb    \n"
+@"   ..oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo...     \n"
+@"   bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb     \n"
+@"   ................................................................      \n"
+@"   bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb      \n"
+@"                                                                         \n"
+@"                                                                         \n"
+@"                                                                         \n"
+@"                                                                         \n"
 ;
 
 static id openDrawerPalette =
@@ -116,6 +122,12 @@ static id textFilePalette =
 @". #000022\n"
 @"X #0055AA\n"
 @"o #FFFFFF\n"
+;
+static id textFileSelectedPalette =
+@"b #000000\n"
+@"o #000022\n"
+@"X #0055AA\n"
+@". #FFFFFF\n"
 ;
 static id textFilePixels =
 @"..............................          \n"
@@ -170,16 +182,20 @@ static id textFilePixels =
 
 @interface AmigaDir : IvarObject
 {
-    time_t _currentDirectoryTimestamp;
+    time_t _timestamp;
     id _array;
     id _buttonDown;
     int _buttonDownOffsetX;
     int _buttonDownOffsetY;
+    id _buttonDownTimestamp;
+    id _selected;
 }
 @end
 @implementation AmigaDir
 - (void)updateFromCurrentDirectory:(Int4)r
 {
+    id bitmap = [[[[@"Bitmap" asClass] alloc] initWithWidth:1 height:1] autorelease];
+    [bitmap useTopazFont];
     id arr = [@"." contentsOfDirectory];
     arr = [arr asFileArray];
     int x = 20;
@@ -188,29 +204,51 @@ static id textFilePixels =
         id elt = [arr nth:i];
         id palette = nil;
         id pixels = nil;
+        id selectedPalette = nil;
+        id selectedPixels = nil;
         id fileType = [elt valueForKey:@"fileType"];
         if ([fileType isEqual:@"file"]) {
             palette = textFilePalette;
             pixels = textFilePixels;
+            selectedPalette = textFileSelectedPalette;
+            selectedPixels = textFilePixels;
         } else if ([fileType isEqual:@"directory"]) {
             palette = drawerPalette;
             pixels = drawerPixels;
+            selectedPalette = drawerPalette;
+            selectedPixels = openDrawerPixels;
         }
         if (!palette || !pixels) {
             continue;
         }
         [elt setValue:palette forKey:@"palette"];
         [elt setValue:pixels forKey:@"pixels"];
+        [elt setValue:selectedPalette forKey:@"selectedPalette"];
+        [elt setValue:selectedPixels forKey:@"selectedPixels"];
         int w = [Definitions widthForCString:[pixels UTF8String]];
         int h = [Definitions heightForCString:[pixels UTF8String]];
-        [elt setValue:nsfmt(@"%d", x) forKey:@"x"];
-        [elt setValue:nsfmt(@"%d", y) forKey:@"y"];
-        [elt setValue:nsfmt(@"%d", w) forKey:@"w"];
-        [elt setValue:nsfmt(@"%d", h) forKey:@"h"];
-        x += w + 20;
-        if (x+w+20 >= r.w) {
-            x = 20;
-            y += h + 30;
+        int textWidth = [Definitions bitmapWidthForText:[elt valueForKey:@"filePath"]];
+        if (textWidth > w) {
+            if (x + textWidth + 5 >= r.w) {
+                x = 20;
+                y += h + 30;
+            }
+            x += (textWidth - w) / 2;
+            [elt setValue:nsfmt(@"%d", x) forKey:@"x"];
+            [elt setValue:nsfmt(@"%d", y) forKey:@"y"];
+            [elt setValue:nsfmt(@"%d", w) forKey:@"w"];
+            [elt setValue:nsfmt(@"%d", h) forKey:@"h"];
+            x += w + ((textWidth - w) / 2) + 20;
+        } else {
+            if (x + w + 5 >= r.w) {
+                x = 20;
+                y += h + 30;
+            }
+            [elt setValue:nsfmt(@"%d", x) forKey:@"x"];
+            [elt setValue:nsfmt(@"%d", y) forKey:@"y"];
+            [elt setValue:nsfmt(@"%d", w) forKey:@"w"];
+            [elt setValue:nsfmt(@"%d", h) forKey:@"h"];
+            x += w + 20;
         }
     }
     [self setValue:arr forKey:@"array"];
@@ -219,15 +257,15 @@ static id textFilePixels =
 - (void)handleBackgroundUpdate:(id)event
 {
     time_t timestamp = [@"." fileModificationTimestamp];
-    if (timestamp != _currentDirectoryTimestamp) {
-        _currentDirectoryTimestamp = 0;
+    if (timestamp != _timestamp) {
+        _timestamp = 0;
     }
 }
 
 - (void)beginIteration:(id)event rect:(Int4)r
 {
-    if (!_currentDirectoryTimestamp) {
-        _currentDirectoryTimestamp = [@"." fileModificationTimestamp];
+    if (!_timestamp) {
+        _timestamp = [@"." fileModificationTimestamp];
         [self updateFromCurrentDirectory:r];
     }
 }
@@ -235,7 +273,7 @@ static id textFilePixels =
 - (void)drawInBitmap:(id)bitmap rect:(Int4)r
 {
     [bitmap useTopazFont];
-    [bitmap setColor:@"blue"];
+    [bitmap setColor:@"#0055aa"];
     [bitmap fillRect:r];
     [bitmap setColor:@"white"];
     for (int i=0; i<[_array count]; i++) {
@@ -244,18 +282,27 @@ static id textFilePixels =
         int y = [elt intValueForKey:@"y"];
         int w = [elt intValueForKey:@"w"];
         int h = [elt intValueForKey:@"h"];
-        id palette = [elt valueForKey:@"palette"];
-        id pixels = [elt valueForKey:@"pixels"];
-        if (palette && pixels) {
-            [bitmap drawCString:[pixels UTF8String] palette:[palette UTF8String] x:r.x+x y:r.y+y];
+        if (_selected == elt) {
+            id palette = [elt valueForKey:@"selectedPalette"];
+            id pixels = [elt valueForKey:@"selectedPixels"];
+            if (palette && pixels) {
+                [bitmap drawCString:[pixels UTF8String] palette:[palette UTF8String] x:r.x+x y:r.y+y];
+            }
+        } else {
+            id palette = [elt valueForKey:@"palette"];
+            id pixels = [elt valueForKey:@"pixels"];
+            if (palette && pixels) {
+                [bitmap drawCString:[pixels UTF8String] palette:[palette UTF8String] x:r.x+x y:r.y+y];
+            }
         }
         id filePath = [elt valueForKey:@"filePath"];
-        [bitmap drawBitmapText:filePath centeredAtX:x+w/2 y:y+h-2];
+        [bitmap drawBitmapText:filePath centeredAtX:x+w/2 y:y+h-8];
     }
 }
 
 - (void)handleMouseDown:(id)event
 {
+    [self setValue:nil forKey:@"selected"];
     int mouseX = [event intValueForKey:@"mouseX"];
     int mouseY = [event intValueForKey:@"mouseY"];
     for (int i=0; i<[_array count]; i++) {
@@ -266,8 +313,27 @@ static id textFilePixels =
         int h = [elt intValueForKey:@"h"];
         if ((mouseX >= x) && (mouseX < x+w) && (mouseY >= y) && (mouseY < y+h)) {
             [self setValue:elt forKey:@"buttonDown"];
+            [self setValue:elt forKey:@"selected"];
             _buttonDownOffsetX = mouseX - x;
             _buttonDownOffsetY = mouseY - y;
+            struct timeval tv;
+            gettimeofday(&tv, NULL);
+            id timestamp = nsfmt(@"%ld.%06ld", tv.tv_sec, tv.tv_usec);
+            if ([timestamp doubleValue] - [_buttonDownTimestamp doubleValue] <= 0.3) {
+                id filePath = [_selected valueForKey:@"filePath"];
+                if ([filePath length]) {
+                    if ([filePath isDirectory]) {
+                        id cmd = nsarr();
+                        [cmd addObject:@"hotdog"];
+                        [cmd addObject:@"amigadir"];
+                        [cmd addObject:filePath];
+                        [cmd runCommandInBackground];
+                    }
+                }
+                [self setValue:nil forKey:@"buttonDownTimestamp"];
+            } else {
+                [self setValue:timestamp forKey:@"buttonDownTimestamp"];
+            }
             break;
         }
     }
@@ -280,6 +346,7 @@ static id textFilePixels =
         int mouseY = [event intValueForKey:@"mouseY"];
         [_buttonDown setValue:nsfmt(@"%d", mouseX - _buttonDownOffsetX) forKey:@"x"];
         [_buttonDown setValue:nsfmt(@"%d", mouseY - _buttonDownOffsetY) forKey:@"y"];
+        [self setValue:nil forKey:@"buttonDownTimestamp"];
     }
 }
 
