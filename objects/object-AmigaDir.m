20eb5f7d4288d9a13e833598fb1c43a4620ae201
Arthur Choung <arthur_choung@me.com>
2021-08-30 06:03:54 +1000

    Draw fuel gauge and scroll bars in AmigaDir

--- objects/object-AmigaDir.m
+++ objects/object-AmigaDir.m
@@ -27,6 +27,278 @@
 
 #include <sys/time.h>
 
+@implementation Definitions(fjeiwofmkdsomvklcxjvlksjdfkjds)
++ (char *)cStringForAmigaHorizontalScrollBarLeft
+{
+    return
+"ooooooooooooooo\n"
+"bbbbbbbbbbbbbbb\n"
+"ooooXXXooooooXX\n"
+"bbbbbbbbbbbbbbb\n"
+"ooXXXooooooooXX\n"
+"bbbbbbbbbbbbbbb\n"
+"XXXooooooooooXX\n"
+"bbbbbbbbbbbbbbb\n"
+"XXXXXXXXXXXooXX\n"
+"bbbbbbbbbbbbbbb\n"
+"XXXooooooooooXX\n"
+"bbbbbbbbbbbbbbb\n"
+"ooXXXooooooooXX\n"
+"bbbbbbbbbbbbbbb\n"
+"ooooXXXooooooXX\n"
+"bbbbbbbbbbbbbbb\n"
+;
+}
++ (char *)cStringForAmigaHorizontalScrollBarMiddle
+{
+    return
+"o\n"
+"b\n"
+"X\n"
+"b\n"
+"o\n"
+"b\n"
+"o\n"
+"b\n"
+"o\n"
+"b\n"
+"o\n"
+"b\n"
+"o\n"
+"b\n"
+"X\n"
+"b\n"
+;
+}
++ (char *)cStringForAmigaHorizontalScrollBarRight
+{
+    return
+"ooooooooooooooo\n"
+"bbbbbbbbbbbbbbb\n"
+"XXoooooooXXXooo\n"
+"bbbbbbbbbbbbbbb\n"
+"XXoooooooooXXXo\n"
+"bbbbbbbbbbbbbbb\n"
+"XXoooooooooooXX\n"
+"bbbbbbbbbbbbbbb\n"
+"XXooXXXXXXXXXXX\n"
+"bbbbbbbbbbbbbbb\n"
+"XXoooooooooooXX\n"
+"bbbbbbbbbbbbbbb\n"
+"XXoooooooooXXXo\n"
+"bbbbbbbbbbbbbbb\n"
+"XXoooooooXXXooo\n"
+"bbbbbbbbbbbbbbb\n"
+;
+}
++ (void)drawAmigaHorizontalScrollBarInBitmap:(id)bitmap x:(int)x0 y:(int)y0 w:(int)w
+{
+    char *palette = [Definitions cStringForAmigaPalette];
+
+    char *left = [Definitions cStringForAmigaHorizontalScrollBarLeft];
+    char *middle = [Definitions cStringForAmigaHorizontalScrollBarMiddle];
+    char *right = [Definitions cStringForAmigaHorizontalScrollBarRight];
+
+    int widthForLeft = [Definitions widthForCString:left];
+    int widthForMiddle = [Definitions widthForCString:middle];
+    int widthForRight = [Definitions widthForCString:right];
+
+    int heightForMiddle = [Definitions heightForCString:middle];
+
+    [bitmap drawCString:left palette:palette x:x0 y:y0];
+    int x;
+    for (x=x0+widthForLeft; x<x0+w-widthForRight; x+=widthForMiddle) {
+        [bitmap drawCString:middle palette:palette x:x y:y0];
+    }
+    [bitmap drawCString:right palette:palette x:x0+w-widthForRight y:y0];
+}
++ (char *)cStringForAmigaFuelGaugeTop
+{
+    return
+"............oo\n"
+"bbbbbbbbbbbbbb\n"
+"...oooooo...oo\n"
+"bbbbbbbbbbbbbb\n"
+"...oo.......oo\n"
+"bbbbbbbbbbbbbb\n"
+"...oooo.....oo\n"
+"bbbbbbbbbbbbbb\n"
+"...oo.......oo\n"
+"bbbbbbbbbbbbbb\n"
+"...oo.......oo\n"
+"bbbbbbbbbbbbbb\n"
+"............oo\n"
+"bbbbbbbbbbbbbb\n"
+;
+}
++ (char *)cStringForAmigaFuelGaugeMiddle
+{
+    return
+"************oo\n"
+"bbbbbbbbbbbbbb\n"
+;
+}
++ (char *)cStringForAmigaFuelGaugeBottom
+{
+    return
+"............oo\n"
+"bbbbbbbbbbbbbb\n"
+"...oooooo...oo\n"
+"bbbbbbbbbbbbbb\n"
+"...oo.......oo\n"
+"bbbbbbbbbbbbbb\n"
+"...oooo.....oo\n"
+"bbbbbbbbbbbbbb\n"
+"...oo.......oo\n"
+"bbbbbbbbbbbbbb\n"
+"...oooooo...oo\n"
+"bbbbbbbbbbbbbb\n"
+"............oo\n"
+"bbbbbbbbbbbbbb\n"
+;
+}
++ (void)drawAmigaFuelGaugeInBitmap:(id)bitmap x:(int)x0 y:(int)y0 h:(int)h
+{
+    char *palette = [Definitions cStringForAmigaPalette];
+
+    char *top = [Definitions cStringForAmigaFuelGaugeTop];
+    char *middle = [Definitions cStringForAmigaFuelGaugeMiddle];
+    char *bottom = [Definitions cStringForAmigaFuelGaugeBottom];
+
+    int heightForTop = [Definitions heightForCString:top];
+    int heightForMiddle = [Definitions heightForCString:middle];
+    int heightForBottom = [Definitions heightForCString:bottom];
+
+    int widthForMiddle = [Definitions widthForCString:middle];
+
+    [bitmap drawCString:top palette:palette x:x0 y:y0];
+    for (int y=y0+heightForTop; y<y0+h-heightForBottom; y+=heightForMiddle) {
+        [bitmap drawCString:middle palette:palette x:x0 y:y];
+    }
+    [bitmap drawCString:bottom palette:palette x:x0 y:y0+h-heightForBottom];
+}
+/*
+FIXME this is the correct one. the one being used has one pixel cut off on the right side
++ (char *)cStringForAmigaVerticalScrollBarTop
+{
+    return
+"oooooXXXXXXoooo\n"
+"bbbbbbbbbbbbbbb\n"
+"oooXXooXXooXXoo\n"
+"bbbbbbbbbbbbbbb\n"
+"oXXooooXXooooXX\n"
+"bbbbbbbbbbbbbbb\n"
+"oooooooXXoooooo\n"
+"bbbbbbbbbbbbbbb\n"
+"oooooooXXoooooo\n"
+"bbbbbbbbbbbbbbb\n"
+"oooooooXXoooooo\n"
+"bbbbbbbbbbbbbbb\n"
+"ooooooooooooooo\n"
+"bbbbbbbbbbbbbbb\n"
+"ooXXXXXXXXXXXXo\n"
+"bbbbbbbbbbbbbbb\n"
+;
+}
++ (char *)cStringForAmigaVerticalScrollBarMiddle
+{
+    return
+"ooXXooooooooXXo\n"
+"bbbbbbbbbbbbbbb\n"
+;
+}
++ (char *)cStringForAmigaVerticalScrollBarBottom
+{
+    return
+"ooXXXXXXXXXXXXo\n"
+"bbbbbbbbbbbbbbb\n"
+"ooooooooooooooo\n"
+"bbbbbbbbbbbbbbb\n"
+"oooooooXXoooooo\n"
+"bbbbbbbbbbbbbbb\n"
+"oooooooXXoooooo\n"
+"bbbbbbbbbbbbbbb\n"
+"oooooooXXoooooo\n"
+"bbbbbbbbbbbbbbb\n"
+"oXXooooXXooooXX\n"
+"bbbbbbbbbbbbbbb\n"
+"oooXXooXXooXXoo\n"
+"bbbbbbbbbbbbbbb\n"
+"oooooXXXXXXoooo\n"
+"bbbbbbbbbbbbbbb\n"
+;
+}
+*/
++ (char *)cStringForAmigaVerticalScrollBarTop
+{
+    return
+"oooooXXXXXXooo\n"
+"bbbbbbbbbbbbbb\n"
+"oooXXooXXooXXo\n"
+"bbbbbbbbbbbbbb\n"
+"oXXooooXXooooX\n"
+"bbbbbbbbbbbbbb\n"
+"oooooooXXooooo\n"
+"bbbbbbbbbbbbbb\n"
+"oooooooXXooooo\n"
+"bbbbbbbbbbbbbb\n"
+"oooooooXXooooo\n"
+"bbbbbbbbbbbbbb\n"
+"oooooooooooooo\n"
+"bbbbbbbbbbbbbb\n"
+"ooXXXXXXXXXXXX\n"
+"bbbbbbbbbbbbbb\n"
+;
+}
++ (char *)cStringForAmigaVerticalScrollBarMiddle
+{
+    return
+"ooXXooooooooXX\n"
+"bbbbbbbbbbbbbb\n"
+;
+}
++ (char *)cStringForAmigaVerticalScrollBarBottom
+{
+    return
+"ooXXXXXXXXXXXX\n"
+"bbbbbbbbbbbbbb\n"
+"oooooooooooooo\n"
+"bbbbbbbbbbbbbb\n"
+"oooooooXXooooo\n"
+"bbbbbbbbbbbbbb\n"
+"oooooooXXooooo\n"
+"bbbbbbbbbbbbbb\n"
+"oooooooXXooooo\n"
+"bbbbbbbbbbbbbb\n"
+"oXXooooXXooooX\n"
+"bbbbbbbbbbbbbb\n"
+"oooXXooXXooXXo\n"
+"bbbbbbbbbbbbbb\n"
+"oooooXXXXXXooo\n"
+"bbbbbbbbbbbbbb\n"
+;
+}
++ (void)drawAmigaVerticalScrollBarInBitmap:(id)bitmap x:(int)x0 y:(int)y0 h:(int)h
+{
+    char *palette = [Definitions cStringForAmigaPalette];
+
+    char *top = [Definitions cStringForAmigaVerticalScrollBarTop];
+    char *middle = [Definitions cStringForAmigaVerticalScrollBarMiddle];
+    char *bottom = [Definitions cStringForAmigaVerticalScrollBarBottom];
+
+    int heightForTop = [Definitions heightForCString:top];
+    int heightForMiddle = [Definitions heightForCString:middle];
+    int heightForBottom = [Definitions heightForCString:bottom];
+
+    int widthForMiddle = [Definitions widthForCString:middle];
+
+    [bitmap drawCString:top palette:palette x:x0 y:y0];
+    for (int y=y0+heightForTop; y<y0+h-heightForBottom; y+=heightForMiddle) {
+        [bitmap drawCString:middle palette:palette x:x0 y:y];
+    }
+    [bitmap drawCString:bottom palette:palette x:x0 y:y0+h-heightForBottom];
+}
+
 static id drawerPalette =
 @"b #000000\n"
 @". #000022\n"
@@ -298,6 +570,9 @@ static id textFilePixels =
         id filePath = [elt valueForKey:@"filePath"];
         [bitmap drawBitmapText:filePath centeredAtX:x+w/2 y:y+h-8];
     }
+    [Definitions drawAmigaFuelGaugeInBitmap:bitmap x:r.x y:r.y h:r.h-16];
+    [Definitions drawAmigaVerticalScrollBarInBitmap:bitmap x:r.x+r.w-14 y:r.y h:r.h-16];
+    [Definitions drawAmigaHorizontalScrollBarInBitmap:bitmap x:r.x y:r.y+r.h-16 w:r.w-14];
 }
 
 - (void)handleMouseDown:(id)event
