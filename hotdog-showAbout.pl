88f6a13a37ef97fc16d0ddcadc03f0c264651407
Arthur Choung <arthur_choung@me.com>
2021-09-24 10:47:39 +1000

    Use amigaalert

--- hotdog-showAbout.pl
+++ hotdog-showAbout.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-if (open FH, "| hotdog alert") {
+if (open FH, "| hotdog amigaalert") {
     print FH <<EOF;
 Horrible Obsolete Typeface and Dreadful Onscreen Graphics for Linux (HOT DOG Linux)
 
