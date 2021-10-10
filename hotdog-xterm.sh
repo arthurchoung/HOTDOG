d0ecbac8eac5f7a0527fa534a70fbe9839e9c12e
Arthur Choung <arthur_choung@me.com>
2021-10-10 18:53:13 +1100

    Use the blue background for Hot Dog Stand mode. It reminds me of Hot Dog On A Stick

--- hotdog-xterm.sh
+++ hotdog-xterm.sh
@@ -2,6 +2,8 @@
 
 if [ "x$HOTDOG_MODE" = "xamiga" ]; then
     xterm -geometry 80x50 -bg '#0055aa' -fg white -cr '#ff8800' +bc +uc
+elif [ "x$HOTDOG_MODE" = "xhotdogstand" ]; then
+    xterm -geometry 80x50 -bg '#0055aa' -fg white -cr '#ffff00' +bc +uc
 else
     xterm -geometry 80x50 -bg white -fg black +bc +uc
 fi
