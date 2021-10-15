48fe5d75e2774c9af333902ee11543bb0336372a
Arthur Choung <arthur_choung@me.com>
2021-10-16 04:08:49 +1100

    Added some ALSA related utils

--- makeUtils.sh
+++ makeUtils.sh
@@ -2,9 +2,12 @@
 
 set -x
 
+gcc -o hotdog-generateALSAPanel hotdog-generateALSAPanel.c -lasound
+gcc -o hotdog-printALSAUpdates hotdog-printALSAUpdates.c -lasound
 gcc -o hotdog-printALSAStatus hotdog-printALSAStatus.c -lasound
 gcc -o hotdog-setALSAMute: hotdog-setALSAMute:.c -lasound
 gcc -o hotdog-setALSAVolume hotdog-setALSAVolume.c -lasound
+gcc -o hotdog-setALSAValues hotdog-setALSAValues.c -lasound
 gcc -o hotdog-monitorBlockDevices hotdog-monitorBlockDevices.c
 gcc -o hotdog-monitorNetworkInterfaces hotdog-monitorNetworkInterfaces.c
 gcc -o hotdog-monitorMonitors hotdog-monitorMonitors.c
