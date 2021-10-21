7edb5e6c05ada79959bd35390f469749b9a2298d
Arthur Choung <arthur_choung@me.com>
2021-10-21 21:21:20 +1100

    Run hotdog-setupWindowManagerMode.sh for enterMacPlatinumMode

--- objects/object-MacPlatinumMenuBar.m
+++ objects/object-MacPlatinumMenuBar.m
@@ -45,6 +45,7 @@
     id menuBar = [windowManager openWindowForObject:[@"MacPlatinumMenuBar" asInstance] x:0 y:0 w:[windowManager intValueForKey:@"rootWindowWidth"] h:[windowManager intValueForKey:@"menuBarHeight"]];
     [windowManager setValue:menuBar forKey:@"menuBar"];
     [windowManager setFocusDict:nil];
+    [@"hotdog-setupWindowManagerMode.sh" runCommandInBackground];
 }
 @end
 
