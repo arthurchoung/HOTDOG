a4fc4c4d0f77295d252e8c5e02c58534614fe658
Arthur Choung <arthur_choung@me.com>
2021-10-21 21:18:08 +1100

    Use black background for Aqua

--- hotdog-xterm.sh
+++ hotdog-xterm.sh
@@ -4,6 +4,8 @@ if [ "x$HOTDOG_MODE" = "xamiga" ]; then
     xterm -geometry 80x50 -bg '#0055aa' -fg white -cr '#ff8800' +bc +uc
 elif [ "x$HOTDOG_MODE" = "xhotdogstand" ]; then
     xterm -geometry 80x50 -bg '#0055aa' -fg white -cr '#ffff00' +bc +uc
+elif [ "x$HOTDOG_MODE" = "xaqua" ]; then
+    xterm -geometry 80x50 -bg black -fg white -cr '#ffff00' +bc +uc
 else
     xterm -geometry 80x50 -bg white -fg black +bc +uc
 fi
