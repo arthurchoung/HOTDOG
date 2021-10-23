03f2cef5f4c1b1af587f99401aa080d59e6e8923
Arthur Choung <arthur_choung@me.com>
2021-10-24 00:33:40 +1100

    Added aqua

--- hotdog-showAbout.pl
+++ hotdog-showAbout.pl
@@ -4,6 +4,9 @@ $arg = '';
 if ($ENV{'HOTDOG_MODE'} eq 'amiga') {
     $arg = 'amiga';
 }
+if ($ENV{'HOTDOG_MODE'} eq 'aqua') {
+    $arg = 'aqua';
+}
 
 if (open FH, "| hotdog ${arg}alert") {
     print FH <<EOF;
