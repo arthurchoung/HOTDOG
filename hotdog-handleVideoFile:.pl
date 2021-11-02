1eaa96e3a17d2032ce541290e65d069582990b70
arthur <arthur@hotdoglinux.com>
2021-11-02 09:56:41 +0000

    Don't use video unscaled

--- hotdog-handleVideoFile:.pl
+++ hotdog-handleVideoFile:.pl
@@ -5,20 +5,10 @@ if (not $path) {
     die('specify file');
 }
 
-if ($path =~ m/2160/) {
-    system
-        'mpv',
-        '--hwdec=auto',
-        '--force-window=yes',
-        '--volume-max=200',
-        '--video-unscaled=yes',
-        $path;
-} else {
-    system
-        'mpv',
-        '--hwdec=auto',
-        '--force-window=yes',
-        '--volume-max=200',
-        $path;
-}
+system
+    'mpv',
+    '--hwdec=auto',
+    '--force-window=yes',
+    '--volume-max=200',
+    $path;
 
