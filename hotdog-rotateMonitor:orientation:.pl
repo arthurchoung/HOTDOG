5eddeaf96f1c4ad161da39e8edc7f7e6ac5213dc
arthur <arthur@darkstar.example.net>
2022-02-18 09:36:38 +0000

    Rotate monitor script

---
+++ hotdog-rotateMonitor:orientation:.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+$matchName = shift @ARGV;
+$orientation = shift @ARGV;
+if (not $matchName or not $orientation) {
+    die('specify matchName and orientation');
+}
+
+$baseDir = `hotdog configDir`;
+chomp $baseDir;
+chdir $baseDir;
+
+system("cat Temp/listMonitors.txt | hotdog-modifyToRotateMonitor:orientation:.pl $matchName $orientation >Temp/monitors.txt");
+
+system('hotdog-setupMonitors.pl');
+
