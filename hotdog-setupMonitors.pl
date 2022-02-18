cc6625bd868bfc5a3bdf18bd47e1b94763a6f8a7
arthur <arthur@darkstar.example.net>
2022-02-18 09:15:17 +0000

    Use txt files instead of csv

--- hotdog-setupMonitors.pl
+++ hotdog-setupMonitors.pl
@@ -4,13 +4,13 @@ $baseDir = `hotdog configDir`;
 chomp $baseDir;
 chdir $baseDir;
 
-system('hotdog-generateSetupMonitorsScriptForCSVFile:.pl Temp/monitors.csv | sh');
+system('cat Temp/monitors.txt | hotdog-generateSetupMonitorsScriptFromMonitorsFile.pl | sh');
 if (not -d 'Temp') {
     system('mkdir', 'Temp');
     system('chmod', '1777', 'Temp');
 }
 
-system('hotdog-listMonitors.pl >Temp/listMonitors.csv');
+system('hotdog-listMonitors.pl >Temp/listMonitors.txt');
 
 # this should be handled with hotdog-setupWindowManagerMode.sh somehow
 if ($ENV{'HOTDOG_MODE'} eq 'aqua') {
