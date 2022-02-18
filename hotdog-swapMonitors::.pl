9088cda6c729fb279a7a082c9a208a2359c56f2b
arthur <arthur@darkstar.example.net>
2022-02-18 09:37:02 +0000

    Script to swap monitors

---
+++ hotdog-swapMonitors::.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+$matchName1 = shift @ARGV;
+$matchName2 = shift @ARGV;
+if (not $matchName1 or not $matchName2) {
+    die('specify matchName1 and matchName2');
+}
+
+if ($matchName1 eq $matchName2) {
+    die('matchName1 and matchName2 are the same');
+}
+
+$baseDir = `hotdog configDir`;
+chomp $baseDir;
+chdir $baseDir;
+
+system("cat Temp/listMonitors.txt | hotdog-modifyToSwapMonitors::.pl $matchName1 $matchName2 >Temp/monitors.txt");
+
+system('hotdog-setupMonitors.pl');
+
