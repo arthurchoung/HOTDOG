a2ec3c19725a7465fb7fac799b19a0a300e0941e
arthur <arthur@darkstar.example.net>
2022-03-06 11:38:51 +0000

    Added button to Sync Time with NTP

--- hotdog-generateDateTimePanel.pl
+++ hotdog-generateDateTimePanel.pl
@@ -4,6 +4,7 @@ print <<EOF;
 panelHorizontalStripes
 panelText:''
 panelText:'This is the DateTimePanel'
-panelCalendarRow:['1' '2' '3' '4' '5' '6' '7']
+panelText:''
+panelButton:'Sync Time with NTP' message:[['ntpdate' 'pool.ntp.org']|runCommandWithSudoInBackground]
 EOF
 
