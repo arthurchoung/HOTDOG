03457d1d987df7a4e9d3a01566dabd8b88911aae
arthur <arthur@darkstar.example.net>
2022-04-10 23:47:04 -0700

    Changed calling of confirm command

--- hotdog-performRestart.pl
+++ hotdog-performRestart.pl
@@ -1,16 +1,8 @@
 #!/usr/bin/perl
 
-$result = undef;
-
-if ($ENV{'HOTDOG_MODE'} eq 'amiga') {
-    $result = `hotdog show "'AmigaAlert'|asInstance|setValue:'Restart?' forKey:'text'|setValue:'Restart' forKey:'okText'|setValue:'Cancel' forKey:'cancelText'"`;
-} elsif ($ENV{'HOTDOG_MODE'} eq 'aqua') {
-    $result = `hotdog show "'AquaAlert'|asInstance|setValue:'Restart?' forKey:'text'|setValue:'Restart' forKey:'okText'|setValue:'Cancel' forKey:'cancelText'"`;
-} else {
-    $result = `echo "Restart?" | hotdog confirm Restart`;
-}
-
+$result = `hotdog confirm Restart Cancel 'Restart?'`;
 chomp $result;
+
 if ($result eq 'Restart') {
     system("sudo -A reboot");
 }
