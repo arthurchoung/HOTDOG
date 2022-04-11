782bfecb72d2511d3b501d35f086971d7838b3a7
arthur <arthur@darkstar.example.net>
2022-04-10 22:02:14 -0700

    Changed calling of hotdog alert

--- hotdog-showAbout.pl
+++ hotdog-showAbout.pl
@@ -1,19 +1,10 @@
 #!/usr/bin/perl
 
-$arg = '';
-if ($ENV{'HOTDOG_MODE'} eq 'amiga') {
-    $arg = 'amiga';
-}
-if ($ENV{'HOTDOG_MODE'} eq 'aqua') {
-    $arg = 'aqua';
-}
-
-if (open FH, "| hotdog ${arg}alert") {
-    print FH <<EOF;
+$str = <<EOF;
 Horrible Obsolete Typeface and Dreadful Onscreen Graphics for Linux (HOT DOG Linux)
 
 Written by Arthur Choung
 EOF
-    close FH;
-}
+
+system('hotdog', 'alert', $str);
 
