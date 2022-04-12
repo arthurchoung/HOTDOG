f50c71aab6026b95350a5a5c612e77cd7c4445a6
arthur <arthur@darkstar.example.net>
2022-04-11 17:27:37 -0700

    Changed calling of password dialog

--- hotdog-getPassword.pl
+++ hotdog-getPassword.pl
@@ -1,7 +1,4 @@
 #!/usr/bin/perl
 
-$|=1;
-
-$output = `hotdog "inputTextWithAlert:'Enter password:' hidden:1" 2>/dev/null`;
-print "$output\n";
+system('hotdog', 'password', 'OK', 'Cancel', 'Enter password:');
 
