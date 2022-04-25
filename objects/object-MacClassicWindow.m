65ea6a6c2f3ff0c32318246c38309628e357304c
arthur <arthur@darkstar.example.net>
2022-04-24 23:25:44 -0700

    Set menuBarHeight when entering Mac Classic mode

--- objects/object-MacClassicWindow.m
+++ objects/object-MacClassicWindow.m
@@ -48,6 +48,7 @@
     [windowManager setValue:rootWindowObject forKey:@"rootWindowObject"];
     [windowManager reparentAllWindows:@"MacClassicWindow"];
     [[windowManager valueForKey:@"menuBar"] setValue:@"1" forKey:@"shouldCloseWindow"];
+    [windowManager setValue:@"20" forKey:@"menuBarHeight"];
     id menuBar = [windowManager openWindowForObject:[@"MacMenuBar" asInstance] x:0 y:0 w:[windowManager intValueForKey:@"rootWindowWidth"] h:[windowManager intValueForKey:@"menuBarHeight"]];
     [windowManager setValue:menuBar forKey:@"menuBar"];
     [windowManager setFocusDict:nil];
