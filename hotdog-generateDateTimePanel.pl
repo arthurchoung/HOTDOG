9966463086e14a6a8ddb2ab9a84d8ee2d6487dad
arthur <arthur@darkstar.example.net>
2022-04-29 02:35:23 -0700

    Show alert with output from ntpdate

--- hotdog-generateDateTimePanel.pl
+++ hotdog-generateDateTimePanel.pl
@@ -5,6 +5,6 @@ panelHorizontalStripes
 panelText:''
 panelText:'This is the DateTimePanel'
 panelText:''
-panelButton:'Sync Time with NTP' message:[['ntpdate' 'pool.ntp.org']|runCommandWithSudoInBackground]
+panelButton:'Sync Time with NTP' message:[['ntpdate' 'pool.ntp.org']|runCommandWithSudoAndReturnOutput|asString|showAlert]
 EOF
 
