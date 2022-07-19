98c14eeda469db8f9eb19c9b4b29d0517e691b7c
arthur <arthur@darkstar.example.net>
2022-07-19 06:01:45 +0000

    Adjust font based on HOTDOG_SCALING

--- hotdog-xterm.sh
+++ hotdog-xterm.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+FONT=""
+if [ $HOTDOG_SCALING -gt 1 ]; then
+    FONT="-fn 12x24"
+fi
+
 if [ "x$HOTDOG_MODE" = "xamiga" ]; then
-    xterm -geometry 80x50 -bg '#0055aa' -fg white -cr '#ff8800' +bc +uc
+    xterm $FONT -geometry 80x50 -bg '#0055aa' -fg white -cr '#ff8800' +bc +uc
 elif [ "x$HOTDOG_MODE" = "xhotdogstand" ]; then
-    xterm -geometry 80x50 -bg '#0055aa' -fg white -cr '#ffff00' +bc +uc
+    xterm $FONT -geometry 80x50 -bg '#0055aa' -fg white -cr '#ffff00' +bc +uc
 elif [ "x$HOTDOG_MODE" = "xaqua" ]; then
-    xterm -geometry 80x50 -bg black -fg white -cr '#ffff00' +bc +uc
+    xterm $FONT -geometry 80x50 -bg black -fg white -cr '#ffff00' +bc +uc
 elif [ "x$HOTDOG_MODE" = "xwinmac" ]; then
-    xterm -geometry 80x50 -bg black -fg white -cr '#ffff00' +bc +uc
+    xterm $FONT -geometry 80x50 -bg black -fg white -cr '#ffff00' +bc +uc
 else
-    xterm -geometry 80x50 -bg white -fg black +bc +uc
+    xterm $FONT -geometry 80x50 -bg white -fg black +bc +uc
 fi
 #    xterm -geometry 80x50 +bc +uc
 
