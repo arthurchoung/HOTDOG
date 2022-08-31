f436fef2620e2ab6e1d8db4bd9ed59657dec7446
arthur <arthur@darkstar.example.net>
2022-08-31 10:41:37 +1000

    Added comment about passwords

--- hotdog-getPassword.pl
+++ hotdog-getPassword.pl
@@ -1,4 +1,10 @@
 #!/usr/bin/perl
 
+# If you are tired of entering your password over and over, and do not care
+# too much about security, and don't mind storing your password in plain text,
+# uncomment the line to print out your password, and comment out the other
+# line, which shows the input dialog.
+
+#print "password";
 system('hotdog', 'password', 'OK', 'Cancel', 'Enter password:');
 
