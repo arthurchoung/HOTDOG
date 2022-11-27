ea8401a0f49b0c28f121fcb0f2691aa75461d08b
arthur <arthur@darkstar.example.net>
2022-11-27 21:26:21 +1100

    Added -useFixedWidthFont

--- objects/object-HotDogStandMenu.m
+++ objects/object-HotDogStandMenu.m
@@ -64,6 +64,15 @@
     }
     return self;
 }
+- (void)useFixedWidthFont
+{
+    id obj = [Definitions scaleFont:_pixelScaling
+                    :[Definitions arrayOfCStringsForAtariSTFont]
+                    :[Definitions arrayOfWidthsForAtariSTFont]
+                    :[Definitions arrayOfHeightsForAtariSTFont]
+                    :[Definitions arrayOfXSpacingsForAtariSTFont]];
+    [self setValue:obj forKey:@"scaledFont"];
+}
 - (int)preferredWidth
 {
     id bitmap = [Definitions bitmapWithWidth:1 height:1];
