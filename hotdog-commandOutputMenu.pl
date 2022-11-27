395075167d79673780c2d09af36a49de6bd30587
arthur <arthur@darkstar.example.net>
2022-11-27 21:34:33 +1100

    Run a command and show the output in a menu

---
+++ hotdog-commandOutputMenu.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+if (not scalar @ARGV) {
+    die('specify command');
+}
+
+$cmd = join ' ', @ARGV;
+
+@output = `$cmd`;
+chomp @output;
+
+print <<EOF;
+displayName,messageForClick
+EOF
+
+$cmd =~ s/'//g;
+$cmd =~ s/"//g;
+
+foreach $elt (@output) {
+    $elt =~ s/'//g;
+    $elt =~ s/"//g;
+    print <<EOF;
+"$elt","NSArray|addObject:'hotdog-handleCommandOutputMenu:line:.pl'|addObject:'$cmd'|addObject:'$elt'|runCommandInBackground"
+EOF
+}
+
