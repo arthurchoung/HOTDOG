aa8638d941556903ae49e1f1bafa34da8de91017
arthur <arthur@darkstar.example.net>
2023-01-06 20:59:09 +1100

    Simple wrapper script for 'df'

--- hotdog-getDiskUsage.pl
+++ hotdog-getDiskUsage.pl
@@ -1,8 +1,15 @@
 #!/usr/bin/perl
 
+$pct = undef;
+
 $output = `df .`;
 if ($output =~ m/([0-9]+)\%/) {
     $pct = $1 / 100.0;
-    print "pct:$pct\n";
 }
+
+if ($output =~ m/([0-9]+)\s+[0-9]+\%/) {
+    $available = $1;
+}
+
+print "pct:$pct available:$available\n";
 
