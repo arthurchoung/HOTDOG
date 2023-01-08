c853ab0a5369b5deff1313e7eb181bc313ba3109
arthur <arthur@darkstar.example.net>
2023-01-09 02:43:21 +1100

    Added *Disk

---
+++ objects/object-AmigaRAMDisk.m
@@ -0,0 +1,229 @@
+/*
+
+ HOT DOG Linux
+
+ Copyright (c) 2020 Arthur Choung. All rights reserved.
+
+ Email: arthur -at- hotdoglinux.com
+
+ This file is part of HOT DOG Linux.
+
+ HOT DOG Linux is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+ */
+
+#import "HOTDOG.h"
+
+#include <sys/time.h>
+
+static char *ramDiskPalette =
+"b #000000\n"
+". #000022\n"
+"X #FF8800\n"
+"o #0055AA\n"
+"O #FFFFFF\n"
+;
+static char *ramDiskSelectedPalette =
+"b #000000\n"
+"O #000022\n"
+"o #FF8800\n"
+"X #0055AA\n"
+". #FFFFFF\n"
+;
+static char *ramDiskPixels =
+"..........................      \n"
+"..........................      \n"
+"..OOOOOOXXXXXX....XXXXOO....    \n"
+"..OOOOOOXXXXXX....XXXXOO....    \n"
+"..OOOOOOXXXXXX....XXXXOOOO....  \n"
+"..OOOOOOXXXXXX....XXXXOOOO....  \n"
+"..OOOOOOXXXXXX....XXXXOOOOOO....\n"
+"..OOOOOOXXXXXX....XXXXOOOOOO....\n"
+"..OOOOOOXXXXXX....XXXXOOOOOOOO..\n"
+"..OOOOOOXXXXXX....XXXXOOOOOOOO..\n"
+"..OOOOOOXXXXXXXXXXXXXXOOOOOOOO..\n"
+"..OOOOOOXXXXXXXXXXXXXXOOOOOOOO..\n"
+"..OOOOOOOOOOOOOOOOOOOOOOOOOOOO..\n"
+"..OOOOOOOOOOOOOOOOOOOOOOOOOOOO..\n"
+"..OOOOOOOOOOOOOOOOOOOOOOOOOOOO..\n"
+"..OOOOOOOOOOOOOOOOOOOOOOOOOOOO..\n"
+"..OOOOOOOOOOOOOOOOOOOOOOOOOOOO..\n"
+"..OOOOOOOOOOOOOOOOOOOOOOOOOOOO..\n"
+"..OOOOOOOOOOOOOOOOOOOOOOOOOOOO..\n"
+"..OOOOOOOOOOOOOOOOOOOOOOOOOOOO..\n"
+"..OOOOOOOOOOOOOOOOOOOOOOOOOOOO..\n"
+"..OOOOOOOOOOOOOOOOOOOOOOOOOOOO..\n"
+"..OOOOOOOOOOOOOOOOOOOOOOOOOOOO..\n"
+"..OOOOOOOOOOOOOOOOOOOOOOOOOOOO..\n"
+"..OOOOOOOOOOOOOOOOOOOOOOOOOOOO..\n"
+"..OOOOOOOOOOOOOOOOOOOOOOOOOOOO..\n"
+"..OOOOOOOOOOOOOOOOOOOOOOOOOOOO..\n"
+"..OOOOOOOOOOOOOOOOOOOOOOOOOOOO..\n"
+"..OOOOOOOOOOOOOOOOOOOOOOOOOOOO..\n"
+"..OOOOOOOOOOOOOOOOOOOOOOOOOOOO..\n"
+"................................\n"
+"................................\n"
+;
+
+
+
+@interface AmigaRAMDisk : IvarObject
+{
+    int _builtin;
+    id _path;
+    id _buttonDown;
+    int _buttonDownX;
+    int _buttonDownY;
+    id _buttonDownTimestamp;
+}
+@end
+@implementation AmigaRAMDisk
+- (int)preferredWidth
+{
+    static int w = 0;
+    if (!w) {
+        w = [Definitions widthForCString:ramDiskPixels];
+        if ([_path length]) {
+            id bitmap = [Definitions bitmapWithWidth:1 height:1];
+            [bitmap useTopazFont];
+            int textWidth = [bitmap bitmapWidthForText:_path];
+            if (textWidth > w) {
+                w = textWidth;
+            }
+        }
+    }
+    return w;
+}
+- (int)preferredHeight
+{
+    static int h = 0;
+    if (!h) {
+        h = [Definitions heightForCString:ramDiskPixels];
+        h += 16;
+    }
+    return h;
+}
+
+- (void)drawInBitmap:(id)bitmap rect:(Int4)r context:(id)context
+{
+    BOOL hasFocus = NO;
+    {
+        id windowManager = [@"windowManager" valueForKey];
+        unsigned long focusInEventWindow = [[windowManager valueForKey:@"focusInEventWindow"] unsignedLongValue];
+        unsigned long win = [[context valueForKey:@"window"] unsignedLongValue];
+        if (focusInEventWindow && (focusInEventWindow == win)) {
+            hasFocus = YES;
+        }
+    }
+
+    int w = [Definitions widthForCString:ramDiskPixels];
+    int h = [Definitions heightForCString:ramDiskPixels];
+
+    if (hasFocus) {
+        [bitmap drawCString:ramDiskPixels palette:ramDiskSelectedPalette x:r.x+(r.w-w)/2 y:r.y];
+    } else {
+        [bitmap drawCString:ramDiskPixels palette:ramDiskPalette x:r.x+(r.w-w)/2 y:r.y];
+    }
+    if ([_path length]) {
+        [bitmap setColor:@"white"];
+        [bitmap useTopazFont];
+        int textWidth = [bitmap bitmapWidthForText:_path];
+        [bitmap drawBitmapText:_path x:r.x+(r.w-textWidth)/2 y:r.y+h];
+    }
+
+    id windowManager = [@"windowManager" valueForKey];
+    unsigned long win = [[context valueForKey:@"window"] unsignedLongValue];
+    if (win) {
+        [windowManager addMaskToWindow:win bitmap:bitmap];
+    }
+}
+
+- (void)handleMouseDown:(id)event
+{
+    {
+        id x11dict = [event valueForKey:@"x11dict"];
+        unsigned long win = [[x11dict valueForKey:@"window"] unsignedLongValue];
+        id windowManager = [@"windowManager" valueForKey];
+        [windowManager XRaiseWindow:win];
+    }
+
+    int mouseX = [event intValueForKey:@"mouseX"];
+    int mouseY = [event intValueForKey:@"mouseY"];
+    _buttonDown = YES;
+    _buttonDownX = mouseX;
+    _buttonDownY = mouseY;
+
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    id timestamp = nsfmt(@"%ld.%06ld", tv.tv_sec, tv.tv_usec);
+    if (_buttonDownTimestamp) {
+        if ([timestamp doubleValue]-[_buttonDownTimestamp doubleValue] <= 0.3) {
+            [self setValue:nil forKey:@"buttonDownTimestamp"];
+            [self handleDoubleClick];
+            return;
+        }
+    }
+    [self setValue:timestamp forKey:@"buttonDownTimestamp"];
+}
+
+- (void)handleMouseMoved:(id)event
+{
+    if (!_buttonDown) {
+        return;
+    }
+    int mouseRootX = [event intValueForKey:@"mouseRootX"];
+    int mouseRootY = [event intValueForKey:@"mouseRootY"];
+
+    id dict = [event valueForKey:@"x11dict"];
+
+    int newX = mouseRootX - _buttonDownX;
+    int newY = mouseRootY - _buttonDownY;
+
+    [dict setValue:nsfmt(@"%d", newX) forKey:@"x"];
+    [dict setValue:nsfmt(@"%d", newY) forKey:@"y"];
+
+    [dict setValue:nsfmt(@"%d %d", newX, newY) forKey:@"moveWindow"];
+}
+- (void)handleMouseUp:(id)event
+{
+    _buttonDown = NO;
+}
+- (void)handleRightMouseDown:(id)event
+{
+    id windowManager = [event valueForKey:@"windowManager"];
+    int mouseRootX = [event intValueForKey:@"mouseRootX"];
+    int mouseRootY = [event intValueForKey:@"mouseRootY"];
+
+    id obj = nil;//[[menuCSV parseCSVFromString] asMenu];
+    if (obj) {
+        [obj setValue:self forKey:@"contextualObject"];
+        [windowManager openButtonDownMenuForObject:obj x:mouseRootX y:mouseRootY w:0 h:0];
+    }
+}
+- (void)handleDoubleClick
+{
+    [self handleOpen];
+}
+- (void)handleOpen
+{
+    if ([_path length]) {
+        id cmd = nsarr();
+        [cmd addObject:@"hotdog"];
+        [cmd addObject:@"amigabuiltindir"];
+        [cmd addObject:@"RAM DISK"];
+        [cmd runCommandInBackground];
+    }
+}
+@end
+
