63acd6dad1b813415ab4cc67624e35b25f90e111
arthur <arthur@darkstar.example.net>
2023-01-12 18:01:47 +1100

    Print out 'used' field

--- hotdog-getDiskUsage.pl
+++ hotdog-getDiskUsage.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
 
 $pct = undef;
+$available = undef;
+$used = undef;
 
 $output = `df .`;
 if ($output =~ m/([0-9]+)\%/) {
@@ -11,5 +13,9 @@ if ($output =~ m/([0-9]+)\s+[0-9]+\%/) {
     $available = $1;
 }
 
-print "pct:$pct available:$available\n";
+if ($output =~ m/([0-9]+)\s+[0-9]+\s+[0-9]+\%/) {
+    $used = $1;
+}
+
+print "pct:$pct available:$available used:$used\n";
 
