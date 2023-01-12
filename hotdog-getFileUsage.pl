8f5c2d1287481469cfee09910d28b1e0eee93d36
arthur <arthur@darkstar.example.net>
2023-01-12 18:02:10 +1100

    If in the '/' directory, use 'df' instead of 'du'.

--- hotdog-getFileUsage.pl
+++ hotdog-getFileUsage.pl
@@ -1,5 +1,18 @@
 #!/usr/bin/perl
 
+$realpath = `realpath .`;
+chomp $realpath;
+if ($realpath eq '/') {
+    $output = `hotdog-getDiskUsage.pl`;
+    chomp $output;
+    if ($output =~ m/\bused:([0-9]+)/) {
+        print "total:$1\n";
+    } else {
+        print "total:\n";
+    }
+    exit 0;
+}
+
 $total = undef;
 
 $output = `du -ksc .`;
