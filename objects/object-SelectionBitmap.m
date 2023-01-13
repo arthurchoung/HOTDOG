0820d41e67cbca99353637c7883a12bf3be38250
arthur <arthur@darkstar.example.net>
2023-01-14 05:39:48 +1100

    Added method to generate a SelectionBitmap

--- objects/object-SelectionBitmap.m
+++ objects/object-SelectionBitmap.m
@@ -25,6 +25,89 @@
 
 #import "HOTDOG.h"
 
+@implementation Definitions(fmkelwmfkldsmkf)
++ (id)selectedBitmapForSelectedItemsInArray:(id)array buttonDownElt:(id)buttonDownElt offsetX:(int)offsetX y:(int)offsetY mouseRootX:(int)mouseRootX y:(int)mouseRootY windowManager:(id)windowManager
+{
+    int selectedCount = 0;
+    int minX = 0;
+    int minY = 0;
+    int maxX = 0;
+    int maxY = 0;
+    for (int i=0; i<[array count]; i++) {
+        id elt = [array nth:i];
+        if (![elt intValueForKey:@"isSelected"]) {
+            continue;
+        }
+        int x = [elt intValueForKey:@"x"];
+        int y = [elt intValueForKey:@"y"];
+        int w = [elt intValueForKey:@"w"];
+        int h = [elt intValueForKey:@"h"];
+        if (!selectedCount) {
+            minX = x;
+            minY = y;
+            maxX = x+w-1;
+            maxY = y+h-1;
+        } else {
+            if (x < minX) {
+                minX = x;
+            }
+            if (y < minY) {
+                minY = y;
+            }
+            if (x+w-1 > maxX) {
+                maxX = x+w-1;
+            }
+            if (y+h-1 > maxY) {
+                maxY = y+h-1;
+            }
+        }
+        selectedCount++;
+    }
+    if (!selectedCount) {
+        return nil;
+    }
+
+    int selectionWidth = maxX - minX + 1;
+    int selectionHeight = maxY - minY + 1;
+
+    id bitmap = [Definitions bitmapWithWidth:selectionWidth height:selectionHeight];
+    id context = nsdict();
+    [context setValue:@"1" forKey:@"isSelected"];
+    for (int i=0; i<[array count]; i++) {
+        id elt = [array nth:i];
+        if (![elt intValueForKey:@"isSelected"]) {
+            continue;
+        }
+        int x = [elt intValueForKey:@"x"];
+        int y = [elt intValueForKey:@"y"];
+        int w = [elt intValueForKey:@"w"];
+        int h = [elt intValueForKey:@"h"];
+        id object = [elt valueForKey:@"object"];
+        Int4 r;
+        r.x = x - minX;
+        r.y = y - minY;
+        r.w = w;
+        r.h = h;
+        if ([object respondsToSelector:@selector(drawInBitmap:rect:context:)]) {
+            [object drawInBitmap:bitmap rect:r context:context];
+        }
+    }
+
+    int x = [buttonDownElt intValueForKey:@"x"] + offsetX - minX;
+    int y = [buttonDownElt intValueForKey:@"y"] + offsetY - minY;
+    int buttonDownOffsetX = x;
+    int buttonDownOffsetY = y;
+
+    id selectionBitmap = [@"SelectionBitmap" asInstance];
+    [selectionBitmap setValue:bitmap forKey:@"bitmap"];
+
+    id newx11dict = [windowManager openWindowForObject:selectionBitmap x:mouseRootX - buttonDownOffsetX y:mouseRootY - buttonDownOffsetY w:selectionWidth h:selectionHeight overrideRedirect:YES];
+    [newx11dict setValue:nsfmt(@"%d", buttonDownOffsetX) forKey:@"buttonDownOffsetX"];
+    [newx11dict setValue:nsfmt(@"%d", buttonDownOffsetY) forKey:@"buttonDownOffsetY"];
+
+    return newx11dict;
+}
+
 @interface SelectionBitmap : IvarObject
 {
     id _bitmap;
