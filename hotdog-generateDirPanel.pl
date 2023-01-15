ba8e80145064264e810a70764e30b483df6e2f00
arthur <arthur@darkstar.example.net>
2023-01-16 05:20:54 +1100

    Use file '00lsflags'. Print flags being used at top of Panel

--- hotdog-generateDirPanel.pl
+++ hotdog-generateDirPanel.pl
@@ -1,13 +1,44 @@
 #!/usr/bin/perl
 
-@arr = `ls --group-directories-first`;
+$arg = shift @ARGV;
+
+$lsflags = '--group-directories-first';
+if (-e '00lsflags') {
+    $lsflags = `cat 00lsflags`;
+    chomp $lsflags;
+    $lsflags =~ s/[^-=a-zA-Z0-9 ]//g;
+}
+
+$cmd = "ls $lsflags";
+@arr = `$cmd`;
 chomp @arr;
+
+if ($arg) {
+    @arr = grep /$arg/i, @arr;
+}
 
 print <<EOF;
 setValue:[ configDir:'Config/filePanelMenu.csv'|parseCSVFile|asMenu ] forKey:'navigationRightMouseDownMessage'
 setValue:[ configDir:'Config/fileMenu.csv'|parseCSVFile|asMenu ] forKey:'buttonRightMouseDownMessage'
 panelHorizontalStripes
 EOF
+
+if ($lsflags) {
+    print <<EOF;
+panelText:'ls: $lsflags'
+EOF
+}
+if ($arg) {
+    $arg =~ s/'/\\'/g;
+    print <<EOF;
+panelText:'grep: $arg'
+EOF
+}
+if ($lsflags || $arg) {
+    print <<EOF;
+panelText:''
+EOF
+}
 
 $numElts = scalar @arr;
 for ($i=0; $i<$numElts; $i++) {
