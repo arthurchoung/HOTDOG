0941a1e17af784b47486062c5d314564aabe9e4f
arthur <arthur@darkstar.example.net>
2023-02-24 01:21:00 +1100

    Added padding

--- objects/object-MacPlatinumRadio.m
+++ objects/object-MacPlatinumRadio.m
@@ -789,7 +789,7 @@ static char *cancelButtonDownRightPixels =
         }
         _rect[i].x = x;
         _rect[i].y = y;
-        text = [bitmap fitBitmapString:text width:r.w-radioWidth-10-(r.x-x)];
+        text = [bitmap fitBitmapString:text width:r.w-radioWidth-10-(x-r.x)-20];
         int textWidth = [bitmap bitmapWidthForText:text];
         int textHeight = [bitmap bitmapHeightForText:text];
         _rect[i].w = radioWidth+10+textWidth;
