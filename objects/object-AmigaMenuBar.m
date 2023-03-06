b39ee59d91f25dd4cf15bf476a5625d93f03b233
arthur <arthur@darkstar.example.net>
2023-03-07 00:48:57 +1100

    Added support for app menu

--- objects/object-AmigaMenuBar.m
+++ objects/object-AmigaMenuBar.m
@@ -105,7 +105,7 @@ static char *titleBarRaiseButtonPixels =
 {
     id _configPath;
     time_t _configTimestamp;
-    int _closingIteration;
+    int _flashIteration;
     BOOL _buttonDown;
     BOOL _rightButtonDown;
     id _selectedDict;
@@ -118,6 +118,11 @@ static char *titleBarRaiseButtonPixels =
     id _textBackgroundPixels;
     id _titleBarRaiseButtonPixels;
     id _titleBarLowerButtonPixels;
+
+    unsigned long _appMenuWindow;
+    int _appMenuWindowX;
+    int _appMenuWindowY;
+    unsigned long _menuWindowWaitForUnmapNotify;
 }
 @end
 
@@ -125,7 +130,7 @@ static char *titleBarRaiseButtonPixels =
 
 - (void)flashIndex:(int)index duration:(int)duration
 {
-    if (_closingIteration > 0) {
+    if (_flashIteration > 0) {
         return;
     }
     if (_selectedDict) {
@@ -135,7 +140,7 @@ static char *titleBarRaiseButtonPixels =
     id dict = [_array nth:index];
     if (dict) {
         [self setValue:dict forKey:@"selectedDict"];
-        _closingIteration = duration;
+        _flashIteration = duration;
     }
 }
 
@@ -178,7 +183,6 @@ static char *titleBarRaiseButtonPixels =
     if (!arr) {
         return;
     }
-    [self setValue:arr forKey:@"array"];
     for (int i=0; i<[arr count]; i++) {
         id elt = [arr nth:i];
         id objectMessage = [elt valueForKey:@"objectMessage"];
@@ -187,6 +191,11 @@ static char *titleBarRaiseButtonPixels =
             [elt setValue:obj forKey:@"object"];
         }
     }
+    id windowManager = [@"windowManager" valueForKey];
+    if (windowManager) {
+        arr = [windowManager incorporateFocusAppMenu:arr];
+    }
+    [self setValue:arr forKey:@"array"];
 }
 - (void)dealloc
 {
@@ -196,21 +205,15 @@ NSLog(@"DEALLOC AmigaMenuBar");
 
 - (BOOL)shouldAnimate
 {
-    if (_closingIteration > 0) {
+    if (_flashIteration > 0) {
         return YES;
     }
     return NO;
 }
 - (void)beginIteration:(id)event rect:(Int4)r
 {
-    if (_closingIteration > 0) {
-        _closingIteration--;
-        id x11dict = [event valueForKey:@"x11dict"];
-        if (_closingIteration == 0) {
-            _buttonDown = NO;
-            [self setValue:nil forKey:@"menuDict"];
-            [self setValue:nil forKey:@"selectedDict"];
-        }
+    if (_flashIteration > 0) {
+        _flashIteration--;
         return;
     }
     time_t timestamp = [_configPath fileModificationTimestamp];
@@ -328,12 +331,14 @@ NSLog(@"AmigaMenuBar handleMouseUp event %@", event);
         return;
     }
 
+    id windowManager = [event valueForKey:@"windowManager"];
+
+    int mouseRootX = [event intValueForKey:@"mouseRootX"];
+    int mouseRootY = [event intValueForKey:@"mouseRootY"];
+
     if (_menuDict) {
-        id windowManager = [event valueForKey:@"windowManager"];
         id object = [_menuDict valueForKey:@"object"];
         if ([object respondsToSelector:@selector(handleMouseUp:)]) {
-            int mouseRootX = [event intValueForKey:@"mouseRootX"];
-            int mouseRootY = [event intValueForKey:@"mouseRootY"];
             int x = [_menuDict intValueForKey:@"x"];
             int y = [_menuDict intValueForKey:@"y"];
             int w = [_menuDict intValueForKey:@"w"];
@@ -341,16 +346,20 @@ NSLog(@"AmigaMenuBar handleMouseUp event %@", event);
             id newEvent = [windowManager generateEventDictRootX:mouseRootX rootY:mouseRootY x:mouseRootX-x y:mouseRootY-y w:w h:h x11dict:_menuDict];
             [object handleMouseUp:newEvent];
             [_menuDict setValue:@"1" forKey:@"needsRedraw"];
-            int closingIteration = [object intValueForKey:@"closingIteration"];
-            if (closingIteration) {
-                _closingIteration = closingIteration;
-                return;
-            }
         }
         [self setValue:nil forKey:@"menuDict"];
     }
     _buttonDown = NO;
     [self setValue:nil forKey:@"selectedDict"];
+
+    if (_appMenuWindow) {
+        unsigned long win = _appMenuWindow;
+        _appMenuWindow = 0;
+        _appMenuWindowX = 0;
+        _appMenuWindowY = 0;
+NSLog(@"handleMouseUp %x XSendButtonReleaseEvent", win);
+        [windowManager XSendButtonReleaseEvent:win button:1];
+    }
 }
 
 - (void)handleMouseMoved:(id)event
@@ -359,9 +368,17 @@ NSLog(@"AmigaMenuBar handleMouseUp event %@", event);
     if (!_buttonDown) {
         return;
     }
+
     id windowManager = [event valueForKey:@"windowManager"];
     int menuBarHeight = [windowManager intValueForKey:@"menuBarHeight"];
     int mouseRootY = [event intValueForKey:@"mouseRootY"];
+
+    if (_appMenuWindow) {
+        int x = mouseRootX - _appMenuWindowX;
+        int y = mouseRootY - _appMenuWindowY;
+        [windowManager XSendMotionEvent:_appMenuWindow x:x y:y rootX:mouseRootX rootY:mouseRootY];
+    }
+
     if (mouseRootY < menuBarHeight) {
         id dict = [self dictForX:mouseRootX];
         if (dict && (dict != _selectedDict)) {
@@ -387,11 +404,63 @@ NSLog(@"AmigaMenuBar handleMouseUp event %@", event);
     }
 
 }
+- (void)mapAppMenu:(id)dict window:(unsigned long)win x:(int)mouseRootX
+{
+    id windowManager = [@"windowManager" valueForKey];
+{
+    if (_appMenuWindow) {
+        if (win == _appMenuWindow) {
+            return;
+        }
+        [windowManager XSendButtonReleaseEvent:_appMenuWindow button:1 x:-1 y:-1 rootX:-1 rootY:-1];
+        [windowManager XUnmapWindow:_appMenuWindow];
+    }
+}
+
+    id monitor = [Definitions monitorForX:mouseRootX y:0];
+    int monitorX = [monitor intValueForKey:@"x"];
+    int monitorWidth = [monitor intValueForKey:@"width"];
+    int x = [dict intValueForKey:@"x"];
+    if (x < 0) {
+        x += monitorX+monitorWidth;
+    } else {
+        x += monitorX;
+    }
+/*
+if (x+w+3 > monitorX+monitorWidth) {
+    int dictWidth = [dict intValueForKey:@"width"];
+    x = x+dictWidth-w-2;
+    if (x < monitorX) {
+        if (w > monitorWidth-3) {
+            x = monitorX;
+            w = monitorWidth-3;
+        } else {
+            x = monitorX+monitorWidth-w-3;
+        }
+    }
+}
+*/
+    [windowManager XMoveWindow:win :x :18];
+    [windowManager XMapWindow:win];
+    [windowManager XRaiseWindow:win];
+    _appMenuWindow = win;
+    _appMenuWindowX = x;
+    _appMenuWindowY = 18;
+    [windowManager XSendButtonPressEvent:win button:1];
+//    id menuDict = [windowManager openWindowForObject:obj x:x y:18*_pixelScaling w:w+3 h:h+3];
+//    [self setValue:menuDict forKey:@"menuDict"];
+//    [self setValue:dict forKey:@"selectedDict"];
+//[windowManager XSetInputFocus:[menuDict unsignedLongValueForKey:@"window"]];
+}
 - (void)openRootMenu:(id)dict x:(int)mouseRootX
 {
-
     id messageForClick = [dict valueForKey:@"messageForClick"];
     if (!messageForClick) {
+        id window = [dict valueForKey:@"window"];
+        if (window) {
+            [self mapAppMenu:dict window:[window unsignedLongValue] x:mouseRootX];
+            return;
+        }
         return;
     }
     id obj = [messageForClick evaluateAsMessage];
@@ -429,6 +498,18 @@ if (x+w+3 > monitorX+monitorWidth) {
         }
     }
 }
+
+{
+    if (_appMenuWindow) {
+        unsigned long appMenuWindow = _appMenuWindow;
+        _appMenuWindow = 0;
+        _appMenuWindowX = 0;
+        _appMenuWindowY = 0;
+        [windowManager XSendButtonReleaseEvent:appMenuWindow button:1 x:-1 y:-1 rootX:-1 rootY:-1];
+        [windowManager XUnmapWindow:appMenuWindow];
+    }
+}
+
     id menuDict = [windowManager openWindowForObject:obj x:x y:18*_pixelScaling w:w+3 h:h+3];
     [self setValue:menuDict forKey:@"menuDict"];
     [self setValue:dict forKey:@"selectedDict"];
@@ -604,7 +685,27 @@ if (x+w+3 > monitorX+monitorWidth) {
         leftPadding *= _pixelScaling;
         int rightPadding = [elt intValueForKey:@"rightPadding"];
         rightPadding *= _pixelScaling;
-        if ((_buttonDown || (_closingIteration > 0)) && (_selectedDict == elt)) {
+
+        int flexible = [elt intValueForKey:@"flexible"];
+        unsigned long window = [elt unsignedLongValueForKey:@"window"];
+
+        BOOL highlight = NO;
+        if (_buttonDown) {
+            highlight = YES;
+        } else if (_rightButtonDown) {
+            highlight = YES;
+        } else if (_flashIteration > 0) {
+            highlight = YES;
+        }
+        if (highlight) {
+            if (_selectedDict == elt) {
+            } else if (_appMenuWindow && (_appMenuWindow == window)) {
+            } else {
+                highlight = NO;
+            }
+        }
+
+        if (highlight) {
             id text = nil;
             if ([obj respondsToSelector:@selector(text)]) {
                 text = [obj text];
@@ -619,7 +720,7 @@ if (x+w+3 > monitorX+monitorWidth) {
                 [bitmap setColor:@"black"];
                 [bitmap fillRect:r2];
                 [bitmap setColorIntR:0xff g:0x88 b:0x00 a:0xff];
-                if (i == flexibleIndex) {
+                if (flexible) {
                     int textWidth = [bitmap bitmapWidthForText:text];
                     if (textWidth > r3.w) {
                         text = [[[bitmap fitBitmapString:text width:r3.w] split:@"\n"] nth:0];
@@ -653,7 +754,7 @@ if (x+w+3 > monitorX+monitorWidth) {
             if (!text) {
                 text = [obj valueForKey:@"text"];
             }
-            if (text) {
+            if ([text length]) {
                 Int4 r3 = r2;
                 r3.x += leftPadding;
                 r3.w -= leftPadding+rightPadding;
