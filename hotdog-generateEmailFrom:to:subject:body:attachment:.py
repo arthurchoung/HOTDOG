4ed378c5bd124fe277c465fb9b9b5615b1a8dceb
arthur <arthur@darkstar.example.net>
2023-03-10 19:29:36 +1100

    Email related scripts

---
+++ hotdog-generateEmailFrom:to:subject:body:attachment:.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.application import MIMEApplication
+
+def get_attachment(path):
+    str = path.lower()
+    name = os.path.basename(path)
+
+    data = open(path, 'rb').read()
+
+    part = None
+    if str.endswith('.jpg') or str.endswith('.jpeg'):
+        part = MIMEImage(data, 'jpeg')
+    elif str.endswith('.png'):
+        part = MIMEImage(data, 'png')
+    elif str.endswith('.gif'):
+        part = MIMEImage(data, 'gif')
+    elif str.endswith('.txt'):
+        part = MIMEText(data, 'plain')
+#    elif str.endswith('.html'):
+#        part = MIMEText(data, 'html')
+    elif str.endswith('.pdf'):
+        part = MIMEApplication(data, 'pdf')
+    else:
+        part = MIMEApplication(data, 'octet-stream')
+
+    part.add_header('Content-Disposition', 'attachment', filename=name)
+    return part
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 5:
+        sys.stderr.write("specify from, to, subject, body, attachment\n")
+        sys.exit(1)
+    fromaddress = sys.argv[1]
+    toaddress = sys.argv[2]
+    subject = sys.argv[3]
+    body = sys.argv[4]
+
+    message = MIMEMultipart()
+    message['From'] = fromaddress
+    message['To'] = toaddress
+    message['Subject'] = subject
+    text = MIMEText(body)
+    message.attach(text)
+
+    for attachmentfile in sys.argv[5:]:
+        attachment = get_attachment(attachmentfile)
+        message.attach(attachment)
+
+    print(message.as_string())
+
---
+++ hotdog-sendEmailFrom:to:subject:body:attachment:.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$#" -lt 4 ]; then
+    echo "specify from, to, subject, body, attachment"
+    exit 1
+fi
+
+RESULT=$( hotdog-generateEmailFrom:to:subject:body:attachment:.py "$@" )
+
+if [ "x$RESULT" == "x" ]; then
+    hotdog alert "unable to generate email"
+    exit 1
+fi
+
+( cat <<EOF
+$RESULT
+EOF
+) | msmtp --tls-certcheck=off "$2"
+
+EXITCODE=$?
+if [ $EXITCODE -ne 0 ]; then
+    hotdog alert "unable to send email"
+    exit 1
+fi
+
+hotdog alert "email probably sent, but you never know with email"
+
