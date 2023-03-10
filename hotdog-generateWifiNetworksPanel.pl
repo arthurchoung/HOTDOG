89b577eb627411eea7c06718512f008c85bf4f95
arthur <arthur@darkstar.example.net>
2023-03-11 02:17:16 +1100

    Show encryption info, reverse numeric sort by quality, rounded buttons for top and bottom

--- hotdog-generateWifiNetworksPanel.pl
+++ hotdog-generateWifiNetworksPanel.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-@lines = `sudo -A hotdog-scanNetworks.pl`;
+@lines = `sudo -A hotdog-scanNetworks.pl | hotdog-reverseNumericSortForKey:.pl quality`;
 chomp @lines;
 
 print <<EOF;
@@ -8,13 +8,18 @@ panelHorizontalStripes
 panelText:'Wireless Networks'
 panelText:''
 panelText:'Choose a network ESSID:'
+panelText:''
 EOF
 
-foreach $line (@lines) {
+$count = scalar @lines;
+for ($i=0; $i<$count; $i++) {
+    $line = $lines[$i];
     $essid = '';
+    $quality = '';
+    $encryption = '';
     if ($line =~ m/\bessid:([^\s]+)/) {
         $essid = $1;
-$essid = "'$essid'";
+#$essid = "'$essid'";
         $essid =~ s/%([0-9a-fA-F]{2})/chr(hex($1))/eg;
         $essid =~ s/\\/\\\\/g;
         $essid =~ s/'/\\'/g;
@@ -22,9 +27,17 @@ $essid = "'$essid'";
     if ($line =~ m/\bquality:([^\s]+)/) {
         $quality = $1;
     }
+    if ($line =~ m/\bencryption:([a-zA-Z]+)/) {
+	$encryption = $1;
+    }
+    $button = 'panelMiddleButton';
+    if ($i == 0) {
+	    $button = 'panelTopButton';
+	} elsif ($i == $count-1) {
+		$button = 'panelBottomButton';
+	}
     print <<EOF;
-panelText:''
-panelButton:'$essid $quality/70' message:['$essid'|writeToStandardOutput;'%20'|percentDecode|writeToStandardOutput;exit:0]
+$button:'$essid [$quality/70] encryption:$encryption' message:['$essid'|writeToStandardOutput;exit:0]
 EOF
 }
 
