7a046e31ffe6c8be8746ce634c867d450f57f5fc
arthur <arthur@darkstar.example.net>
2023-03-24 21:44:17 +1100

    Remove wireless check, use prgbox for output

--- hotdog-connectNetworkInterface.pl
+++ hotdog-connectNetworkInterface.pl
@@ -12,47 +12,28 @@ if ($dhcpcd) {
     exit 1;
 }
 
-`sudo -A ifconfig $interface up`;
+system('sudo', '-A', 'ifconfig', $interface, 'up');
 
-$wireless = 0;
-open(FH, "iwconfig $interface |") or die('unable to run iwconfig');
-while ($line = <FH>) {
-    if ($line =~ m/^$interface/) {
-        if ($line =~ m/ESSID:/) {
-            $wireless = 1;
-        }
-    }
-}
-close(FH);
+system('hotdog', 'prgbox', 'sudo', '-A', 'dhcpcd', '-4', $interface);
 
-if ($wireless) {
-    $essid = `sudo -A hotdog-scanNetworks.pl | hotdog choose '#{essid|percentDecode} #{quality}/70' 'Wireless Network' '\nChoose a network ESSID:\n\n' | hotdog-allValuesForKey:.pl essid | hotdog-percentDecode.pl | hotdog-quotedString.pl`;
-    chomp $essid;
-    if (not $essid) {
-        exit 0;
-    }
-
-    `sudo -A iwconfig $interface essid $essid`;
-}
-
-if (open FH, "sudo -A dhcpcd -4 $interface 2>&1 | hotdog progress |") {
-    $addr = undef;
-    while ($line = <FH>) {
-        chomp $line;
-        if ($line =~ m/ leased ([\d\.]+)/) {
-            $addr = $1;
-        }
-    }
-    close(FH);
-    if ($addr) {
-        system('hotdog', 'alert', "Obtained address $addr");
-    } else {
-        $dhcpcd = `pgrep -f 'dhcpcd.*$interface'`;
-        chomp $dhcpcd;
-        if ($dhcpcd) {
-            `sudo -A kill $dhcpcd`;
-        }
-        system('hotdog', 'alert', 'Unable to obtain address');
-    }
-}
+#if (open FH, "sudo -A dhcpcd -4 $interface 2>&1 | hotdog progress |") {
+#    $addr = undef;
+#    while ($line = <FH>) {
+#        chomp $line;
+#        if ($line =~ m/ leased ([\d\.]+)/) {
+#            $addr = $1;
+#        }
+#    }
+#    close(FH);
+#    if ($addr) {
+#        system('hotdog', 'alert', "Obtained address $addr");
+#    } else {
+#        $dhcpcd = `pgrep -f 'dhcpcd.*$interface'`;
+#        chomp $dhcpcd;
+#        if ($dhcpcd) {
+#            `sudo -A kill $dhcpcd`;
+#        }
+#        system('hotdog', 'alert', 'Unable to obtain address');
+#    }
+#}
 
