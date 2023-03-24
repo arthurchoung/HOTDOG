64ee1445e6aad796ded2481e45b5c1dd989c3ae9
arthur <arthur@darkstar.example.net>
2023-03-24 21:48:21 +1100

    Simple script to run wpa_supplicant

---
+++ hotdog-runWPASupplicantForInterface:essid:password:.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+$interface = shift @ARGV;
+$essid = shift @ARGV;
+$password = shift @ARGV;
+if (not $interface or not $essid or not $password) {
+    die('specify interface, essid, and password');
+}
+
+$essid =~ s/\n//g;
+$essid =~ s/\\/\\\\/g;
+$essid =~ s/"/\\"/g;
+
+$password =~ s/\n//g;
+$password =~ s/\\/\\\\/g;
+$password =~ s/"/\\"/g;
+
+$baseDir = `hotdog configDir`;
+chomp $baseDir;
+chdir $baseDir;
+
+$configFile = "Temp/wpa_supplicant.conf";
+open FH, ">$configFile" or die('unable to write file $configFile');
+
+print FH <<EOF;
+ctrl_interface=/var/run/wpa_supplicant
+ctrl_interface_group=root
+
+network={
+    ssid="$essid"
+    psk="$password"
+}
+EOF
+
+close FH;
+
+system('hotdog', 'prgbox', 'sudo', '-A', 'wpa_supplicant', '-i', $interface, '-c', $configFile);
+
