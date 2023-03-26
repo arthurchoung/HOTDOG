b54f7eab791eea109dc30319b360e67830e70317
arthur <arthur@darkstar.example.net>
2023-03-26 22:44:53 +1100

    Added gettimeofday

---
+++ lib/lib-time.m
@@ -0,0 +1,38 @@
+/*
+
+ HOT DOG Linux
+
+ Copyright (c) 2020 Arthur Choung. All rights reserved.
+
+ Email: arthur -at- hotdoglinux.com
+
+ This file is part of HOT DOG Linux.
+
+ HOT DOG Linux is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+ */
+
+#import "HOTDOG.h"
+
+#include <sys/time.h>
+
+@implementation Definitions(mfkelwmfklsdmklfm)
++ (id)gettimeofday
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return nsfmt(@"%ld.%06ld", tv.tv_sec, tv.tv_usec);
+}
+@end
+
