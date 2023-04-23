b1c7dfe4fe550c8c89b1f7c1040fff41ae482506
arthur <arthur@darkstar.example.net>
2023-04-23 19:48:46 +1000

    Allow negative index for flashIndex:duration:

--- objects/object-HotDogStandMenuBar.m
+++ objects/object-HotDogStandMenuBar.m
@@ -89,7 +89,11 @@ static char *menuBarButtonsPixels =
 
 - (void)flashIndex:(int)index duration:(int)duration
 {
-    _flashIndex = index;
+    if (index < 0) {
+        _flashIndex = [_array count] + index;
+    } else {
+        _flashIndex = index;
+    }
     _flashIteration = duration;
 }
 
