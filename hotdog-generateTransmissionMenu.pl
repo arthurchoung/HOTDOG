1ddf9ac860733aff67b1cabf9577078a40c0cad4
arthur <arthur@darkstar.example.net>
2023-04-23 19:53:23 +1000

    Use key val syntax to eliminate escaping

--- hotdog-generateTransmissionMenu.pl
+++ hotdog-generateTransmissionMenu.pl
@@ -1,32 +1,35 @@
 #!/usr/bin/perl
 
-print <<EOF;
-displayName,messageForClick
-EOF
-
 $pid = `pidof transmission-daemon`;
 chomp $pid;
 if ($pid) {
     print <<EOF;
-"Open Web Interface","'chromium http://admin:password\@127.0.0.1:9091'|split|runCommandInBackground"
-,
-"Stop Daemon (pid $pid)","'kill $pid'|split|runCommandInBackground"
+=displayName=Open Web Interface
+=messageForClick='chromium http://admin:password\@127.0.0.1:9091'|split|runCommandInBackground
+==
+==
+=pid=$pid
+=stringFormat=Stop Daemon (pid #{pid})
+=messageForClick=NSArray|addObject:'kill'|addObject:pid|runCommandInBackground
+==
 EOF
     $clip = `xclip -o`;
     if ($clip) {
-        $clip =~ s/\\/\\\\/g;
-        $clip =~ s/'/\\'/g;
-        $clip =~ s/"/\\"/g;
         if ($clip =~ m/^magnet:/) {
             print <<EOF;
-,
-"Add $clip","['transmission-remote' '-n' 'admin:password' '-a' '$clip']|runCommandAndReturnOutput|asString|showAlert"
+==
+=clip=$clip
+=stringFormat=Add #{clip}
+=messageForClick=NSArray|addObject:'transmission-remote'|addObject:'-n'|addObject:'admin:password'|addObject:'-a'|addObject:clip|runCommandAndReturnOutput|asString|showAlert
+==
 EOF
         }
     }
 } else {
     print <<EOF;
-"Start Daemon","'transmission-daemon -t -u admin -v password -p 9091 -a 127.0.0.1'|split|runCommandInBackground"
+=displayName=Start Daemon
+=messageForClick='transmission-daemon -t -u admin -v password -p 9091 -a 127.0.0.1'|split|runCommandInBackground
+==
 EOF
 }
 
