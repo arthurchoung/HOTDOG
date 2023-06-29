b434a3f023b89be517ed181cd98f63a5ca718912
arthur <arthur@darkstar.example.net>
2023-06-30 00:32:13 +1200

    Added Clipboard menu

---
+++ hotdog-generateClipboardMenu.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+print <<EOF;
+displayName,stringFormat,messageForClick
+,"#{NSArray|addObject:'xclip'|addObject:'-o'|runCommandAndReturnOutput|asString}"
+"Test showAlert",,"NSArray|addObject:'hotdog'|addObject:'alert'|addObject:(NSArray|addObject:'xclip'|addObject:'-o'|runCommandAndReturnOutput|asString)|runCommandInBackground"
+,,
+EOF
+
+$clip = `xclip -o`;
+
+if ($clip =~ m/([0-9\.\-\+]+)/) {
+    $num = $1;
+    $inchToCM = $num * 2.54;
+    $mileToKM = $num * 1.6;
+    $lbToKG = $num * 0.45;
+    $cmToInch = $num * 0.39;
+    $kmToMile = $num * 0.6;
+    $kgToPound = $num * 2.2;
+    $celsiusToFahrenheit = (9.0/5.0)*$num + 32.0;
+    $fahrenheitToCelsius = (5.0/9.0)*($num - 32);
+    print <<EOF;
+,"$num in =~ $inchToCM cm",
+,"$num cm =~ $cmToInch in",
+,"$num mile =~ $mileToKM km",
+,"$num km =~ $kmToMile mile",
+,"$num lb =~ $lbToKG kg",
+,"$num kg =~ $kgToPound lb",
+,"$num celsius =~ $celsiusToFahrenheit fahrenheit",
+,"$num fahrenheit =~ $fahrenheitToCelsius celsius",
+EOF
+}
+
