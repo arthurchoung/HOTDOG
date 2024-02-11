1bba4a66a17f5e03a358724400e40a53b0b7ee35
arthur <arthur@darkstar.example.net>
2024-02-12 00:46:37 +1300

    Added red potion and brown potion

--- objects/object-BitmapMenuItem.m
+++ objects/object-BitmapMenuItem.m
@@ -25,6 +25,91 @@
 
 #import "HOTDOG.h"
 
+static char *redPotionPalette =
+". #E09C20\n"
+"X #B03020\n"
+"o #F8FCF8\n"
+"O #ffffff\n"
+"b #000000\n"
+;
+static char *redPotionPixels =
+"                \n"
+"      bbbb      \n"
+"     b....b     \n"
+"      bXXb      \n"
+"     boooob     \n"
+"     boXXob     \n"
+"     bobbob     \n"
+"     bobbob     \n"
+"    boobboob    \n"
+"   bobbbbbbob   \n"
+"   bobbbbbbob   \n"
+"   boXXXXXXob   \n"
+"   boXXXXX.ob   \n"
+"   boX.XXXXob   \n"
+"   boXXX.XXob   \n"
+"   booXXXXoob   \n"
+"   boooooooob   \n"
+"    boooooob    \n"
+"     bbbbbb     \n"
+;
+@implementation Definitions(fnmjkdfsjkfsdjkeklwfmklsdmfksdkfmdfjkjkfjdksjf)
++ (id)RedPotionMenuItem
+{
+    id pixels = nsfmt(@"%s", redPotionPixels);
+    id palette = nsfmt(@"%s", redPotionPalette);
+    id highlightedPalette = palette;
+
+    id obj = [@"BitmapMenuItem" asInstance];
+    [obj setValue:pixels forKey:@"pixels"];
+    [obj setValue:palette forKey:@"palette"];
+    [obj setValue:highlightedPalette forKey:@"highlightedPalette"];
+    return obj;
+}
+@end
+static char *brownPotionPalette =
+". #E09C20\n"
+"X #a3826c\n"
+"o #F8FCF8\n"
+"O #ffffff\n"
+"b #000000\n"
+;
+static char *brownPotionPixels =
+"                \n"
+"      bbbb      \n"
+"     b....b     \n"
+"      bXXb      \n"
+"     boooob     \n"
+"     boXXob     \n"
+"     boXXob     \n"
+"     boXXob     \n"
+"    booXXoob    \n"
+"   boXXXXXXob   \n"
+"   boXXXXXXob   \n"
+"   boXXXXXXob   \n"
+"   boXXXXX.ob   \n"
+"   boX.XXXXob   \n"
+"   boXXX.XXob   \n"
+"   boXXXXXXob   \n"
+"   booXXXXoob   \n"
+"    boooooob    \n"
+"     bbbbbb     \n"
+;
+@implementation Definitions(fnmjkdfsjkfsdjkeklwfmklsdmfksdkfmdfjkjk)
++ (id)BrownPotionMenuItem
+{
+    id pixels = nsfmt(@"%s", brownPotionPixels);
+    id palette = nsfmt(@"%s", brownPotionPalette);
+    id highlightedPalette = palette;
+
+    id obj = [@"BitmapMenuItem" asInstance];
+    [obj setValue:pixels forKey:@"pixels"];
+    [obj setValue:palette forKey:@"palette"];
+    [obj setValue:highlightedPalette forKey:@"highlightedPalette"];
+    return obj;
+}
+@end
+
 static char *macComputerMenuIconPalette =
 "b #000000\n"
 ". #222222\n"
