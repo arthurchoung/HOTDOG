c4b03ecc10bc1ffb6f006147ab5ef3c332e4f514
arthur <arthur@darkstar.example.net>
2024-02-12 00:47:54 +1300

    Renamed generateAppMenuArray to appMenuArray

--- objects/object-MacPlatinumDesktop.m
+++ objects/object-MacPlatinumDesktop.m
@@ -178,7 +178,7 @@ exit(1);
 NSLog(@"handleDestroyNotifyEvent:%@", event);
     [self exit:0];
 }
-- (id)generateAppMenuArray
+- (id)appMenuArray
 {
     id fileMenu = [[fileMenuCSV parseCSVFromString] asMenu];
     [fileMenu setValue:@"File" forKey:@"title"];
