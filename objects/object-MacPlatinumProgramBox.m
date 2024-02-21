1541682d8271dff9977a4da7978593f4712b6aa5
arthur <arthur@darkstar.example.net>
2024-02-21 19:09:17 +1300

    Handle return key

--- objects/object-MacPlatinumProgramBox.m
+++ objects/object-MacPlatinumProgramBox.m
@@ -655,6 +655,9 @@ static char *okButtonDownRightPixels =
     if ([keyString isEqual:@"return"]) {
         if (_returnKeyDown) {
             if (_eof) {
+                if (_dialogMode) {
+                    exit(0);
+                }
                 id x11dict = [event valueForKey:@"x11dict"];
                 [x11dict setValue:@"1" forKey:@"shouldCloseWindow"];
             }
