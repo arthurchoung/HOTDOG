76e78b35a48cf484c167d5bca4f18f565d23445e
arthur <arthur@hotdoglinux.com>
2024-05-13 02:46:50 +1200

    Updated for FreeBSD

--- objects/object-TextInputDialog.m
+++ objects/object-TextInputDialog.m
@@ -271,7 +271,7 @@ static void drawAlertBorderInBitmap_rect_(id bitmap, Int4 r)
 }
 
 
-#ifdef BUILD_FOR_LINUX
+#if defined(BUILD_FOR_LINUX) || defined(BUILD_FOR_FREEBSD)
 @implementation Definitions(fjdklsjflkdsjlkfdsljkfkjsdmvcxki)
 + (id)inputNumberWithAlert:(id)text
 {
