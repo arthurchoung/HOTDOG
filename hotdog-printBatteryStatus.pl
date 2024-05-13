728e87e9f87e079ecf1dcdcb2463ecb6bacb0c05
arthur <arthur@hotdoglinux.com>
2024-05-13 23:59:41 +1200

    Updated for FreeBSD

--- hotdog-printBatteryStatus.pl
+++ hotdog-printBatteryStatus.pl
@@ -1,30 +1,33 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
 $|=1;
 
 for (;;) {
-    $chargeFullDesign = `cat /sys/class/power_supply/BAT0/charge_full_design 2>/dev/null`;
-    #$chargeFullDesign = `cat /sys/class/power_supply/BAT0/energy_full_design 2>/dev/null`;
-    chomp $chargeFullDesign;
-    $chargeFull = `cat /sys/class/power_supply/BAT0/charge_full 2>/dev/null`;
-    #$chargeFull = `cat /sys/class/power_supply/BAT0/energy_full 2>/dev/null`;
-    chomp $chargeFull;
-    $chargeNow = `cat /sys/class/power_supply/BAT0/charge_now 2>/dev/null`;
-    #$chargeNow = `cat /sys/class/power_supply/BAT0/energy_now 2>/dev/null`;
-    chomp $chargeNow;
-    $status = `cat /sys/class/power_supply/BAT0/status 2>/dev/null`;
-    chomp $status;
+    @lines = `acpiconf -i 0`;
+    chomp @lines;
 
-    $str = 'No battery';
+    $state = '';
+    $remaining = '';
+    foreach $line (@lines) {
+        if ($line =~ m/^State:\s+(.+)/) {
+            $state = $1;
+        }
+        if ($line =~ m/^Remaining capacity:\s+(.+)/) {
+            $remaining = $1;
+        }
+    }
 
-    if ($status && $chargeNow && $chargeFullDesign) {
-        $str = sprintf("%s: %.0f%%", $status, ($chargeNow / $chargeFullDesign) * 100.0);
-    } elsif ($status) {
-        $str = $status;
+    $str = 'Battery';
+    if ($state eq 'high') {
+        $str = "Charged: $remaining";
+    } elsif ($state eq 'discharging') {
+        $str = "Discharging: $remaining";
+    } elsif (not $state) {
+        $str = 'No battery';
     }
 
     print "$str\n";
 
-    sleep 5;
+    sleep 10;
 }
 
