44abc2f642c48dde738ea98b93d9c606b8ee7806
arthur <arthur@hotdoglinux.com>
2024-05-14 20:26:47 +1200

    Cleanup

--- Landfill/Ubuntu/hotdogbuntu-macclassic.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-exec /usr/bin/hotdog "runWindowManager:'enterMacClassicMode'"
-
--- Landfill/hotdog-openALSAPanel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "x$1" = "x" ]; then
-    echo "specify alsa device, example 'hw:0'"
-    exit 1
-fi
-
-hotdog-generateALSAPanel "$@" | hotdog show ALSAPanel | hotdog-setALSAValues "$1"
-
