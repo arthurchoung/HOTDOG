724311d47e19e23c1d8e52e0fd50d0fffa98dc06
arthur <arthur@hotdoglinux.com>
2024-05-31 02:21:18 +1200

    Linux

--- linux/linux-audio.m
+++ linux/linux-audio.m
@@ -25,6 +25,8 @@
 
 #import "HOTDOG.h"
 
+#ifdef BUILD_FOR_FREEBSD
+
 #include <fcntl.h>
 #include <sys/soundcard.h>
 
@@ -51,7 +53,13 @@
 
 - (void)openAudioWithSampleRate:(int)sampleRate frameCount:(int)frameCount channels:(int)channels bitsPerChannel:(int)bitsPerChannel
 {
-    if (bitsPerChannel != 16) {
+    int fmt = 0;
+
+    if (bitsPerChannel == 16) {
+        fmt = AFMT_S16_LE;
+    } else if (bitsPerChannel == 8) {
+        fmt = AFMT_S8;
+    } else {
 NSLog(@"error, %d bits per channel not supported", bitsPerChannel);
         exit(1);
     }
@@ -66,13 +74,8 @@ NSLog(@"unable to open audio device '%s", DEFAULT_AUDIO_DEVICE);
         exit(1);
     }
 
-    int fmt = AFMT_S16_LE;
     if (ioctl(_fd, SNDCTL_DSP_SETFMT, &fmt) == -1) {
 NSLog(@"unable to set audio format");
-        exit(1);
-    }
-    if (fmt != AFMT_S16_LE) {
-NSLog(@"error, audio device does not support s16 le");
         exit(1);
     }
 
@@ -96,7 +99,7 @@ NSLog(@"unable to set audio sample rate");
     if (_fd < 0) {
         return;
     }
-    int len = frameCount * 2 * _numberOfChannels;
+    int len = frameCount * (_bitsPerChannel/8) * _numberOfChannels;
     int n = write(_fd, buf, len);
     if (n != len) {
 NSLog(@"unable to write to audio device, frameCount %d result %d", frameCount, n);
@@ -111,4 +114,50 @@ NSLog(@"unable to write to audio device, frameCount %d result %d", frameCount, n
     }
 }
 @end
+
+#endif
+
+#ifdef BUILD_FOR_LINUX
+@interface AudioOutput : IvarObject
+{
+    id _aplay;
+    int _sampleRate;
+    int _numberOfChannels;
+    int _bitsPerChannel;
+}
+@end
+@implementation AudioOutput
+
+- (void)openAudioWithSampleRate:(int)sampleRate frameCount:(int)frameCount channels:(int)channels bitsPerChannel:(int)bitsPerChannel
+{
+    _sampleRate = sampleRate;
+    _numberOfChannels = channels;
+    _bitsPerChannel = bitsPerChannel;
+    
+    id cmd = nsarr();
+    [cmd addObject:@"aplay"];
+    [cmd addObject:@"-t"];
+    [cmd addObject:@"raw"];
+    [cmd addObject:@"-r"];
+    [cmd addObject:nsfmt(@"%d", sampleRate)];
+    [cmd addObject:@"-c"];
+    [cmd addObject:nsfmt(@"%d", channels)];
+    [cmd addObject:@"-f"];
+    [cmd addObject:@"S16_LE"];
+    [cmd addObject:@"-"];
+    id aplay = [cmd runCommandAndReturnProcess];
+    [self setValue:aplay forKey:@"aplay"];
+}   
+
+- (void)writeAudio:(uint16_t *)buffer frameCount:(int)frameCount
+{
+    [_aplay writeBytes:buffer length:_numberOfChannels*(_bitsPerChannel/8)*frameCount];
+}
+
+- (void)closeAudio
+{
+    [self setValue:nil forKey:@"aplay"];
+}
+@end
+#endif
 
