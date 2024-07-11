462925bca0870f10b1994abc0d8ed72a1cc49772
arthur <arthur@hotdoglinux.com>
2024-07-11 13:24:28 +1200

    Take screenshot for content of window

---
+++ hotdog-takeScreenshotForContentOfWindow:.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+
+$arg = shift @ARGV;
+if (not $arg) {
+    die("specify window id");
+}
+
+$date = `date "+%Y-%m-%d %H:%M"`;
+chomp $date;
+
+$base = "$ENV{'HOME'}/Screenshot $date";
+$path = "$base.png";
+for ($i=0;;$i++) {
+    if (-e $path) {
+        $path = "$base-$i.png";
+        next;
+    }
+    last;
+}
+
+system(qq{import -window "$arg" "$path"});
+if ($? != 0) {
+    system('hotdog', 'alert', 'Unable to take screen shot');
+    exit 0;
+}
+
+system('hotdog', 'alert', "Screen shot is located at '$path'");
+
