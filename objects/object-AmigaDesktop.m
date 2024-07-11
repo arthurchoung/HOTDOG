8180364b64b16911bc6047bec8cc18c19e962a8e
arthur <arthur@hotdoglinux.com>
2024-07-11 13:22:54 +1200

    Forgot to revert changes

--- objects/object-AmigaDesktop.m
+++ objects/object-AmigaDesktop.m
@@ -139,7 +139,7 @@ NSLog(@"objectPath %@", objectPath);
 + (id)AmigaDesktop
 {
     id observercmd = nsarr();
-    [observercmd addObject:@"hotdog-monitorMountChanges.pl"];
+    [observercmd addObject:@"hotdog-monitorMountChanges"];
     id observer = [observercmd runCommandAndReturnProcess];
     if (!observer) {
 NSLog(@"unable to run observer command %@", observercmd);
@@ -237,7 +237,7 @@ NSLog(@"windowManager %@", windowManager);
     id objectWindows = [windowManager valueForKey:@"objectWindows"];
 
     id cmd = nsarr();
-    [cmd addObject:@"hotdog-listDisks.pl"];
+    [cmd addObject:@"hotdog-listBlockDevices.pl"];
     id blockDevicesLines = [[[cmd runCommandAndReturnOutput] asString] split:@"\n"];
     id lines = nsarr();
     [lines addObject:@"builtin:AmigaRAMDiskIcon mountpoint:RAM%20Disk"];
